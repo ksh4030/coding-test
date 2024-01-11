743a0c98fa9d8a1feefae1309f6300822df0a5ea
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-01-11 10:21:08 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 577.69 ms, Memory: 186 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] elements) {
+        int[] newElements = new int[elements.length * 2];
+        
+        for(int i = 0; i < elements.length; i++) {
+            newElements[i] = newElements[i + elements.length] = elements[i];
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 1; i <= elements.length; i++) {
+            for(int j = 0; j < elements.length; j++) {
+                set.add(Arrays.stream(newElements, j, j+i).sum());
+            }
+        }
+        
+        return set.size();
+    }
+}
