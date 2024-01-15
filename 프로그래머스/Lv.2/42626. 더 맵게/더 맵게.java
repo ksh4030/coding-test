f48e6ee97d96b6c213951d559a98e371d832af10
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-01-16 00:25:44 +0900

    [level Lv.2] Title: 더 맵게, Time: 1611.95 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int n : scoville) {
+            pq.add(n);
+        }
+
+        int min = pq.peek();
+
+        while (min < K) {
+            if(pq.size() >= 2) {
+                pq.add(pq.poll() + (pq.poll() * 2));
+                min = pq.peek();
+                answer++;
+            } else {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
