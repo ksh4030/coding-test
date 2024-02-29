d5365f85a023a296740787f225a92a06c9252a72
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-02-29 12:12:23 +0900

    [level 3] Title: 스티커 모으기(2), Time: 5.72 ms, Memory: 56.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int sticker[]) {
+        int answer = 0;
+        
+        if(sticker.length == 1) return sticker[0];
+        
+        int[] arr1 = new int[sticker.length];
+        int[] arr2 = new int[sticker.length];
+
+        arr1[0] = sticker[0];
+        arr1[1] = sticker[0];
+        for (int i = 2; i < sticker.length-1; i++) {
+            arr1[i] = Math.max(arr1[i-2] + sticker[i], arr1[i-1]);
+        }
+
+        arr2[0] = 0;
+        arr2[1] = sticker[1];
+        for (int i = 2; i < sticker.length; i++) {
+            arr2[i] = Math.max(arr2[i-2] + sticker[i], arr2[i-1]);
+        }
+
+        answer = Math.max(arr1[arr1.length-2], arr2[arr2.length-1]);
+
+        return answer;
+    }
+}
