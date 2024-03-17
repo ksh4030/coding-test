dbb0c011daf6513e528f2ab6689defff555531e8
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-03-17 21:42:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N==1) {
+            System.out.println(1);
+            return;
+        } else if (N==2) {
+            System.out.println(2);
+            return;
+        }
+
+        long[] arr = new long[N+1];
+        arr[1]=1;
+        arr[2]=2;
+        for(int i=3; i<=N; i++) {
+            arr[i] = (arr[i-1] + arr[i-2])  % 10_007;
+        }
+
+        System.out.println(arr[N]);
+    }
+}
