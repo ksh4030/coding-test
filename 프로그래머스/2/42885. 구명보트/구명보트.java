cff7f322a19973e5b8f7fbdc633e0393be3d4415
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-05-09 12:36:40 +0900

    [level 2] Title: 구명보트, Time: 8.46 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,9 +3,7 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         int min = 0;
-
         Arrays.sort(people);
-
         for (int max = people.length-1; max >= min; max--) {
             if(people[min] + people[max] <= limit) {
                 min++;
