93b89891df0360d12adfaf15a1016791b57c5ca5
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-06-19 17:27:46 +0900

    [level 3] Title: 거스름돈, Time: 10.19 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+class Solution {
+    static final int MOD = 1_000_000_007;
+    public int solution(int n, int[] money) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        
+        for(int coin : money) {
+            for(int i=coin; i<=n; i++) {
+                arr[i] = (arr[i]+arr[i-coin]) % MOD;
+            }
+        }
+        
+        return arr[n];
+    }
+}
