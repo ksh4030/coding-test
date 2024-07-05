bf82221cf951d316275e8fecc03609a25f98fb14
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-07-05 18:08:41 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.16 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,43 +1,47 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        int friendsLenght = friends.length;
-        HashMap<String, Integer> dic = new HashMap<>();
-        int[] giftDegree = new int[friendsLenght];
-        int[][] giftGraph = new int[friendsLenght][friendsLenght];
+        Map<String, Integer> map = new HashMap<>();
+        int[][] board = new int[friends.length][friends.length];
+        int[] score = new int[friends.length];
+        int[] ans = new int[friends.length];
         
-        for ( int i = 0; i < friendsLenght; i++ ) {
-            dic.put(friends[i], i);
+        for(int i=0; i<friends.length; i++) map.put(friends[i], i);        
+        for(int i=0; i<gifts.length; i++) {
+            String[] bucket = gifts[i].split(" ");
+            board[map.get(bucket[0])][map.get(bucket[1])] += 1;
         }
         
-        for ( String gift : gifts ) {
-            String[] giftName = gift.split(" ");
-            giftDegree[dic.get(giftName[0])]++;
-            giftDegree[dic.get(giftName[1])]--;
-            giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;    
+        for(int i=0; i<friends.length; i++) {
+            int r = 0;
+            int c = 0;
+            for(int j=0; j<friends.length; j++) {
+                r += board[i][j];
+                c += board[j][i];
+            }            
+            score[i] = r-c;
         }
         
-        for ( int i =0; i< friendsLenght; i++) {
-            int num = 0;
-            for ( int j = 0; j< friendsLenght; j++) {
-                if ( i == j) {
-                    continue;
-                }    
-                
-                if (giftGraph[i][j] > giftGraph[j][i] ||
-                     (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
-                        num++;
+        for(int i=0; i<friends.length; i++) {
+            for(int j=i+1; j<friends.length; j++) {
+                if(board[i][j] > board[j][i]){
+                    ans[i]++;
+                } else if(board[i][j] < board[j][i]) {
+                    ans[j]++;
+                } else {
+                    if(score[i] > score[j]) {
+                        ans[i]++;
+                    } else if(score[i] < score[j]) {
+                        ans[j]++;
                     }
+                }
             }
-            
-            if ( answer < num) {
-                answer = num;
-            }
-            
         }
-        
+        // System.out.println(Arrays.deepToString(board));
+        // System.out.println(Arrays.toString(ans));
+        answer = Arrays.stream(ans).max().orElseThrow();
         return answer;
     }
 }
