6d87dd035f9c33286a50e4fe1496e840963803ba
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-07-12 22:00:09 +0900

    [level 1] Title: 폰켓몬, Time: 6.24 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,9 +1,10 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
         Set<Integer> set = new HashSet<>();
-        for(int n : nums) set.add(n);
-        return Math.min(nums.length/2, set.size());
+        for(int n : nums) set.add(n);        
+        answer = nums.length/2 > set.size() ? set.size() : nums.length/2;
+        return answer;
     }
 }
