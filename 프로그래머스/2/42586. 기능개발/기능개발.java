34c338b6ce5c252c28ec17e7ec28da489b975e54
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-07-16 17:28:33 +0900

    [level 2] Title: 기능개발, Time: 0.29 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,44 +1,31 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int[] progresses, int[] speeds) {
+    public int[] solution(int[] progresses, int[] speeds) {        
         Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> sq = new LinkedList<>();
         List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < progresses.length; i++) {
-            q.add(progresses[i]);
-            sq.add(speeds[i]);
+        
+        for(int i=0; i<progresses.length; i++) {
+            int n = (100-progresses[i]) % speeds[i] > 0 ? (100-progresses[i]) / speeds[i] + 1 : (100-progresses[i]) / speeds[i];
+            q.add(n);
         }
-
-        while (!q.isEmpty()) {
-            int cnt = 0;
-            for (int i = 0; i < q.size(); i++) {
-                int cur = q.poll();
-                int curSpeed = sq.poll();
-
-                q.add(cur + curSpeed);
-                sq.add(curSpeed);
-            }
-
-            while (!q.isEmpty()) {
-                if(q.peek() >= 100) {
+        
+        while(!q.isEmpty()) {
+            int n = 1;
+            int cur = q.poll();
+            while(true) {
+                if(!q.isEmpty() && q.peek() <= cur) {
+                    n++;
                     q.poll();
-                    sq.poll();
-                    cnt++;
-                    if(q.isEmpty()) {
-                        list.add(cnt);
-                    }
                 } else {
-                    if(cnt > 0) {
-                        list.add(cnt);
-                    }
                     break;
                 }
             }
+            list.add(n);
         }
         
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for(int i=0; i<answer.length; i++) {
             answer[i] = list.get(i);
         }
         return answer;
