cec22435d1eb249f7ffe43066cc2f437aeb1e884
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-07-16 16:40:50 +0900

    [level 2] Title: 미로 탈출, Time: 18.69 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,75 +1,68 @@
 import java.util.*;
 class Solution {
-    static String[][] map;
-    static boolean[][] v;
     static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static int target;
-    static Node bucket;
+    static int[] dc = {0, -1, 0, 1};
+    static boolean[][] v;
+    static Node start, bucket;
+    static String[][] map;
     public int solution(String[] maps) {
         int answer = 0;
+        v = new boolean[maps.length][maps[0].length()];        
         map = new String[maps.length][maps[0].length()];
-        target = 0;
-        bucket = null;
-
-        Node start = null;
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length(); j++) {
-                map[i][j] = String.valueOf(maps[i].charAt(j));
+        
+        for(int i=0; i<maps.length; i++) {
+            map[i] = maps[i].split("");
+        }
+        
+        for(int i=0; i<maps.length; i++) {
+            for(int j=0; j<maps[0].length(); j++) {
                 if(map[i][j].equals("S")) {
                     start = new Node(i,j,0);
+                    break;
                 }
             }
         }
-
-        maze(start, "L");
-        int check = target;
-        if(bucket != null) {
-            maze(bucket, "E");
-            if(target == check) {
-                return -1;
-            }
-        }
-        if(target == 0) {
-            return -1;
-        }
-        return target;
+        
+        int n = bfs(start, "L");
+        if(n == -1) return -1;
+        answer += n;
+        
+        v = new boolean[maps.length][maps[0].length()];
+        n = bfs(bucket, "E");
+        if(n == -1) return -1;
+        answer += n;
+        
+        return answer;
     }
     
-    public static void maze(Node node, String s) {
-        v = new boolean[map.length][map[0].length];
+    public static int bfs(Node node, String target) {
         Queue<Node> q = new LinkedList<>();
         q.add(node);
-
-        while (!q.isEmpty()) {
+        v[node.r][node.c] = true;
+        
+        while(!q.isEmpty()) {
             Node cur = q.poll();
-
-            v[cur.r][cur.c] = true;
-            for (int i = 0; i < 4; i++) {
+            
+            if(map[cur.r][cur.c].equals(target)) {
+                if(target.equals("L")) bucket = new Node(cur.r, cur.c, 0);
+                return cur.cnt;
+            }      
+            
+            for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
-
-                try {
-                    if (map[nr][nc].equals(s)) {
-                        bucket = new Node(nr, nc, cur.cnt+1);
-                        target = cur.cnt+1;
-                        return;
-                    }
-                    if(!v[nr][nc] && !map[nr][nc].equals("X")) {
-                        v[nr][nc] = true;
-                        q.add(new Node(nr, nc, cur.cnt+1));
-                    }
-                } catch (Exception e) {
-                    continue;
-                }
+                
+                if(nr<0 || nc<0 || nr>=v.length || nc>=v[0].length || v[nr][nc] || map[nr][nc].equals("X")) continue;
+                v[nr][nc] = true;
+                q.add(new Node(nr, nc, cur.cnt+1));
             }
-        }
-
+        }        
+        return -1;
     }
     
-    static public class Node {
-        int r,c,cnt;
-        public Node(int r, int c, int cnt) {
+    static class Node {
+        int r, c, cnt;
+        Node(int r, int c, int cnt) {
             this.r = r;
             this.c = c;
             this.cnt = cnt;
