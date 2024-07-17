43f23edec50bca8c632127ec1f3b5966fb5a5868
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-07-17 18:02:12 +0900

    [level 2] Title: 주식가격, Time: 19.95 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
         for (int i = 0; i < prices.length; i++) {
             for (int j = i+1; j < prices.length; j++) {
                 answer[i]++;
