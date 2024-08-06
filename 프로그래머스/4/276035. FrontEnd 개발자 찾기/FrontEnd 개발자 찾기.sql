5ec65626da178043d3d27f31150726f403e50579
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-08-06 20:37:51 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 작성해주세요
+select id, email, first_name, last_name
+from developers
+where skill_code & (
+    select sum(code)
+    from skillcodes
+    where category = 'Front end'
+)
+order by id;
