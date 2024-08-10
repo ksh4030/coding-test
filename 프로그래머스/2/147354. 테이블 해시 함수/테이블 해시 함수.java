d4f47f11ddd1ca25c737eb1184d83eba31f37447
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-08-10 21:10:46 +0900

    [level 2] Title: 테이블 해시 함수, Time: 10.09 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,14 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
+        Arrays.sort(data, (int[] o1, int[] o2) -> {
+            if(o1[col-1] == o2[col-1]) return o2[0] - o1[0];
+            return o1[col-1] - o2[col-1];
+        });
+        
         int answer = 0;
-        Arrays.sort(data, (o1, o2) -> o1[col-1] == o2[col-1] ? o2[0]-o1[0] : o1[col-1]-o2[col-1]);
-
-        for (int i = row_begin-1; i <= row_end-1; i++) {
-            int bucket = 0;
-            for (int n : data[i]) bucket += (n%(i+1));
-            answer = answer ^ bucket;
+        
+        for(int i=row_begin-1; i<row_end; i++) {
+            int num = 0;
+            for(int n : data[i]) {
+                num += (n % (i+1));
+            }
+            answer = answer ^ num;
         }
+        
         return answer;
     }
 }
