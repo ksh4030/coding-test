e168d5784d4cab512c56f4763bf0cdaf50881ac1
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-08-15 23:01:31 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,52 +1,41 @@
+import java.util.*;
 class Solution {
-    static String[] arr;
     public int solution(int storey) {
         int answer = 0;
-        arr = Integer.toString(storey).split("");
-
-        String cur = arr[arr.length-1];
-
-        for (int i = arr.length-2; i >= 0; i--) {
-            if(Integer.parseInt(cur) > 5) {
-                answer += 10 - Integer.parseInt(cur);
-                arr[i] = Integer.toString(Integer.parseInt(arr[i]) + 1);
-            } else if (Integer.parseInt(cur) < 5) {
-                answer += Integer.parseInt(cur);
+        int[] arr = new int[Integer.toString(storey).length()];
+        
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = storey%10;
+            storey /= 10;
+        }
+        
+        for(int i=0; i<arr.length-1; i++) {
+            if(arr[i] == 10) {
+                arr[i+1]++;
+            }else if(arr[i] < 5) {
+                answer += arr[i];
+            } else if (arr[i] > 5) {
+                answer += (10 - arr[i]);
+                arr[i+1]++;
             } else {
-                answer+=5;
-                if(Integer.parseInt(arr[i]) >= 5) {
-                    arr[i] = Integer.toString(Integer.parseInt(arr[i]) + 1);
+                if(arr[i+1] >= 10) {
+                    answer += 5;
+                } else if(arr[i+1] >= 5) {
+                    answer += 5;
+                    arr[i+1]++;
+                } else {
+                    answer += arr[i];                    
                 }
             }
-
-            if(arr[i].length() >= 2) {
-                cal(i);
-            }
-            cur = arr[i];
         }
-
-        if(arr[0].length() >= 2) {
+        
+        if(arr[arr.length-1] == 10) {
             answer++;
-            cur = String.valueOf(arr[0].charAt(1));
+        } else if (arr[arr.length-1] >= 6) {
+            answer += (10-arr[arr.length-1]+1);
         } else {
-            cur = arr[0];
-        }
-
-        if(Integer.parseInt(cur) > 5) {
-            answer += 10 - Integer.parseInt(cur) + 1;
-        } else {
-            answer += Integer.parseInt(cur);
+            answer += arr[arr.length-1];
         }
         return answer;
-    }
-    public static void cal(int i) {
-        for(int j=i; j>=1; j--) {
-            if(arr[j].length() == 1) {
-                break;
-            }
-
-            arr[j] = Integer.toString(Integer.parseInt(arr[j]) - 10);
-            arr[j-1] = Integer.toString(Integer.parseInt(arr[j-1]) + 1);
-        }
     }
 }
