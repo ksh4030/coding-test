3b22de9f36b230f4125c15d16b20d65e8c0466e6
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-08-16 15:28:27 +0900

    [level 2] Title: 124 나라의 숫자, Time: 6.09 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,14 +1,27 @@
+import java.util.*;
 class Solution {
-    public String solution(int n) {
-        String answer = "";
-        String[] arr = {"4", "1", "2"};
-        while (n > 0) {
-            int nmj = n % 3;
-            n /= 3;
-            if(nmj == 0) n--;
-
-            answer = arr[nmj] + answer;
+    public String solution(int n) {        
+        List<String> list = new ArrayList<>();
+        while(n >= 1) {            
+            switch(n%3) {
+                case 0 :
+                    list.add("4");
+                    n = n/3 - 1;
+                    break;
+                case 1:
+                    list.add("1");
+                    n /= 3;
+                    break;
+                case 2:
+                    list.add("2");
+                    n/=3;
+                    break;
+            }
         }
+        
+        String answer = "";
+        Collections.reverse(list);
+        for(String s : list) answer += s;
         return answer;
     }
 }
