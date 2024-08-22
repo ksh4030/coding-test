97c7e1e675fe204aacf7cc646bd48550c2622a65
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-08-22 16:01:35 +0900

    [level 2] Title: 양궁대회, Time: 4.00 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,44 +1,52 @@
 public class Solution {
-	static private int[] res = new int[11];//점수차가 최대일때 라이언이 쏜 화살배열
-	static private int[] lion= {-1};//정답배열
-	static private int max = Integer.MIN_VALUE;//최대값
+    static private int[] res = new int[11]; 
+    static private int[] lion = {-1}; 
+    static private int max = Integer.MIN_VALUE; 
+
     public static int[] solution(int n, int[] info) {
-        back(0,n,info);//라이언이 쏜 화살에 대한 조합
-        
-        if(max==-1) {//라이언이 어피치를 못 이길떄
-        	lion = new int[1];
-        	lion[0]=-1;
+        back(0, n, info, 0); 
+        if (max <= 0) { 
+            return new int[]{-1};
         }
         return lion;
     }
-    
-    public static void back(int depth, int n, int[] info) {
-    	if(depth==n) {
-    		int diff = score(info);
-    		if(max<=diff) {
-    			max = diff;
-    			lion = res.clone();
-    		}
-    		return;
-    	}
-    	
-    	for(int i=0; i<info.length && res[i]<=info[i]; i++) {
-    		res[i] += 1;
-    		back(depth+1, n, info);
-    		res[i] -= 1;
-    	}
+
+    public static void back(int depth, int n, int[] info, int start) {
+        if (depth == n) { 
+            int diff = score(info);
+            if (diff > max) { 
+                max = diff;
+                lion = res.clone();
+            } else if (diff == max) { 
+                for (int i = 10; i >= 0; i--) {
+                    if (res[i] != lion[i]) {
+                        if (res[i] > lion[i]) lion = res.clone();
+                        break;
+                    }
+                }
+            }
+            return;
+        }
+
+        
+        for (int i = start; i < 11; i++) {
+            if (res[i] > info[i]) continue; 
+
+            res[i]++;
+            back(depth + 1, n, info, i); 
+            res[i]--;
+        }
     }
-    
+
     public static int score(int[] info) {
-    	int apeach=0, lion=0;
-    	for(int i=0; i<res.length; i++) {
-    		if(info[i]==0 && res[i]==0) continue;
-    		if(info[i]>=res[i]) apeach += (10-i);
-    		else lion += (10-i);
-    	}
-    	
-    	int diff = lion - apeach;
-    	if(diff<=0) return -1;
-    	return diff;
+        int apeach = 0, lionScore = 0;
+        for (int i = 0; i < 11; i++) {
+            if (info[i] == 0 && res[i] == 0) continue; 
+            if (info[i] >= res[i]) apeach += (10 - i); 
+            else lionScore += (10 - i); 
+        }
+
+        int diff = lionScore - apeach;
+        return diff > 0 ? diff : -1; 
     }
 }
