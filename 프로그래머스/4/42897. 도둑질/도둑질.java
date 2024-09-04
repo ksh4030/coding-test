58eb5fafa3745fe54a3d3a1b0485dee30df6290b
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-09-04 16:34:23 +0900

    [level 4] Title: 도둑질, Time: 28.16 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] money) {
+        int[] dp1 = new int[money.length]; 
+        int[] dp2 = new int[money.length]; 
+        int len = money.length;
+
+        dp1[0] = money[0];
+        dp1[1] = money[0];
+
+        dp2[0] = 0;
+        dp2[1] = money[1];
+
+        for (int i = 2; i < len; i++) {
+            dp1[i] = Math.max(dp1[i - 1], money[i] + dp1[i - 2]);
+            dp2[i] = Math.max(dp2[i - 1], money[i] + dp2[i - 2]);
+        }
+        return Math.max(dp1[len - 2], dp2[len - 1]);
+    }
+}
