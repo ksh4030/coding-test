410e31913a3243adf14335281d4ce39047dc0c99
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-12 16:46:54 +0900

    [level 3] Title: 등굣길, Time: 0.96 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,20 +1,28 @@
+import java.util.*;
 class Solution {
-    static final int MOD = 1_000_000_007;
+    final int MOD = 1_000_000_007;
+    int[][] map;
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        int[][] map = new int[n+1][m+1];
-        for (int i = 0; i < puddles.length; i++) {
+        map = new int[n+1][m+1];
+        map[1][1] = 1;
+        
+        for(int i=0; i<puddles.length; i++) {
             map[puddles[i][1]][puddles[i][0]] = -1;
         }
-
-        map[1][1] = 1;
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < m+1; j++) {
-                if (map[i][j] == -1) continue;
-                if (map[i-1][j]>0) map[i][j]+=map[i-1][j]%MOD;
-                if (map[i][j-1]>0) map[i][j]+=map[i][j-1]%MOD;
+        
+        for(int i=1; i<map.length; i++) {
+            for(int j=1; j<map[i].length; j++) {
+                if(i==1 && j==1) continue;
+                if(map[i][j] == -1) continue;
+                
+                map[i][j] = ((map[i-1][j] == -1 ? 0 : map[i-1][j]) + (map[i][j-1] == -1 ? 0 : map[i][j-1])) % MOD;
             }
         }
-        return map[n][m]%MOD;
+        answer = map[n][m];
+        // for(int[] bucket : map) {
+        //     System.out.println(Arrays.toString(bucket));
+        // }
+        return answer;
     }
 }
