2082a5fd1d87e816779af1fcfb9d776383c2c3a6
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-13 18:36:58 +0900

    [level 3] Title: 징검다리 건너기, Time: 54.72 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,27 +1,29 @@
-class Solution {
-   public int solution(int[] stones, int k) {
+import java.util.*;
+class Solution {    
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        int max = 200_000_000;
         int min = 0;
-        int max = Integer.MAX_VALUE;
-        int result = 0;
+        
         while(min <= max) {
-            int mid = (min + max) / 2;
-            if(check(mid, k, stones)) {
-                min = mid + 1;
-                result = mid;
+            int mid = (max+min)/2;
+            if(check(mid, stones, k)) {
+                answer = Math.max(answer, mid);
+                min = mid+1;
             } else {
-                max = mid - 1;
+                max = mid-1;
             }
         }
-        return result;
+        
+        return answer;
     }
     
-    public boolean check(int mid, int k, int[] stones) {
-        int count = 0;
-        for(int i = 0; i < stones.length; i++) {
-            if(stones[i] < mid) { 
-                count++;
-                if(count >= k) return false;
-            } else count = 0;
+    public boolean check(int mid, int[] stones, int k) {
+        int cnt = 0;
+        for(int i=0; i<stones.length; i++) {
+            if(stones[i] < mid) cnt++;
+            else cnt = 0;
+            if(cnt == k) return false;
         }
         return true;
     }
