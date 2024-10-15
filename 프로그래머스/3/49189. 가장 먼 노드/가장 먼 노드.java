3152733aab5216542aa15af384019ff8a3faa2b7
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-15 23:50:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 56.75 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,52 +1,58 @@
 import java.util.*;
 
 class Solution {
-    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    //인접 리스트에 그래프 저장
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visit;
+    
     public int solution(int n, int[][] edge) {
-        for(int i = 0 ; i <= n ; i++) graph.add(new ArrayList<>());
-		
-		for (int[] i : edge) {
-			int v = i[0];
-			int w = i[1];
-			graph.get(v).add(w);
-			graph.get(w).add(v);
-		}
-
-		boolean[] visit = new boolean[n + 1];
-        return bfs(graph, n, visit);
+        int answer = 0;
+        visit = new boolean[n+1];
+        
+        for(int i=0; i<=n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] arr : edge) {
+            graph.get(arr[0]).add(arr[1]);
+            graph.get(arr[1]).add(arr[0]);
+        }
+        
+        answer = bfs();
+        
+        return answer;
     }
     
-    public static int bfs(ArrayList<ArrayList<Integer>> graph, int n, boolean[] visit) {
-		Queue<int[]> q = new LinkedList<>();
-		int answer = 0;
-		
-		q.add(new int[] {1, 0});
-		visit[1] = true;
-		int maxDepth = 0;
-		
-		while(!q.isEmpty()) {
-			int[] arr = q.poll();
-			int v = arr[0];
-			int depth = arr[1];
-			
+    public static int bfs() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(1, 0));
+        visit[1] = true;
+        int max = 0;
+        int ans = 0;
+        
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
             
-			if(maxDepth == depth) answer++; // 최대 길이 노드라면 answer++;
-			else if (maxDepth < depth) { // 더 긴 거리에 노드가 있다면 answer = 1, MaxDepth 갱신
-				maxDepth = depth;
-				answer = 1;
-			}
-
-			
-			for (int i = 0; i < graph.get(v).size(); i++) {
-				int w = graph.get(v).get(i); //인접 정점
-				if (!visit[w]) {
-					q.add(new int[] { w, depth + 1 });
-					visit[w] = true;
-				}
-			}
-		}
-
-		return answer;
-	}
+            if(cur.cnt == max) ans++;
+            if(cur.cnt > max) {
+                ans = 1;
+                max = cur.cnt;
+            }
+            
+            for(int i=0; i<graph.get(cur.num).size(); i++) {
+                if(!visit[graph.get(cur.num).get(i)]){
+                    q.add(new Node(graph.get(cur.num).get(i), cur.cnt + 1));
+                    visit[graph.get(cur.num).get(i)] = true;
+                }
+            }
+        }
+        return ans;
+    }
+    
+    static class Node {
+        int num, cnt;
+        public Node(int num, int cnt){
+            this.num = num;
+            this.cnt = cnt;            
+        }
+    }
 }
