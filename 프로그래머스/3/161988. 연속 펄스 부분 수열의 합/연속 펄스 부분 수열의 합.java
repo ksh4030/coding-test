2f31d1b9ff073fc3be15ecad9aad5d74656109fb
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-16 22:24:54 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 21.93 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,29 +1,33 @@
 import java.util.*;
 class Solution {
-    static long max;
     public long solution(int[] sequence) {
         long answer = 0;
-        max = 0;
-        int[] a = sequence.clone();
-        int[] b = sequence.clone();
+        
+        int[] a = new int[sequence.length];
+        int[] b = new int[sequence.length];
         
         for(int i=0; i<sequence.length; i++) {
             if(i%2 == 0) {
-                a[i] *= -1;
+                a[i] = sequence[i];
+                b[i] = sequence[i] * -1;
             } else {
-                b[i] *= -1;
+                a[i] = sequence[i] * -1;
+                b[i] = sequence[i];
             }
-        }
-        kadane(a);
-        kadane(b);
-        return max;
+        }        
+        answer = Math.max(kadane(a), kadane(b));        
+        return answer;
     }
     
-    public static void kadane(int[] arr) {
+    public static long kadane(int[] arr) {
+        long max = 0;
         long cur = 0;
+        
         for(int i=0; i<arr.length; i++) {
             cur = Math.max(arr[i], cur+arr[i]);
             max = Math.max(cur, max);
         }
+        
+        return max;
     }
 }
