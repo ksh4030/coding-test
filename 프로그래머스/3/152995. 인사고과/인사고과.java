4e6498afb6df1f4432d9275e0165f93aa1010b43
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-18 22:48:16 +0900

    [level 3] Title: 인사고과, Time: 141.46 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -1,40 +1,32 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] scores) {
-        int answer = 0;
-        int x = scores[0][0];
-        int y = scores[0][1];
+        int answer = 1;
+        int[] target = {scores[0][0], scores[0][1]};
         
-        Arrays.sort(scores, (o1,o2) -> {
-            return o1[0] == o2[0] ? o1[1]-o2[1] : o2[0] - o1[0];
+        Arrays.sort(scores, (o1, o2) -> {
+            return o1[0]==o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
         });
         
-        int standard = scores[0][1];
+        int num = scores[0][1];
         for(int i=1; i<scores.length; i++) {
-            if(scores[i][1] < standard) {
-                if(scores[i][0]==x && scores[i][1]==y) return -1;
-                scores[i][0] = -1;
-                scores[i][1] = -1;
+            if(num > scores[i][1]) {
+                if(scores[i][0]==target[0] && scores[i][1]==target[1]) return -1;
+                scores[i][0] = scores[i][1] = -1;
             } else {
-                standard = scores[i][1];
+                num = scores[i][1];
             }
         }
         
         Arrays.sort(scores, (o1, o2) -> {
-            return (o2[0]+o2[1]) - (o1[0]+o1[1]);
-        });
-        
-        // System.out.println(Arrays.deepToString(scores));
-        // System.out.println(x+y);
+           return (o2[0]+o2[1]) - (o1[0]+o1[1]);
+        });   
         
         for(int i=0; i<scores.length; i++) {
-            if(scores[i][0] + scores[i][1] > x+y) {
-                answer++;
-            } else {
-                break;
-            } 
+            if(scores[i][0] + scores[i][1] > target[0] + target[1]) answer++;
+            else break;
         }
         
-        return answer+1;
+        return answer;
     }
 }
