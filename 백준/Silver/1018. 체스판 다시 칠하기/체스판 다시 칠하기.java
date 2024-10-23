9a8260ba1df7dfaefa486dedea2113869f9ecbb2
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-23 21:13:26 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,63 +1,56 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    int[] nr = {-1, 0, 1, 0};
-    int[] nc = {0 , 1, 0, -1};
-
-    static String[][] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int answer = Integer.MAX_VALUE;
-
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        map = new String[R][C];
-
-        for(int i=0; i<R; i++) {
-            map[i] = br.readLine().split("");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        char[][] board = new char[N][M];
+        
+        // 보드 입력 받기
+        for (int i = 0; i < N; i++) {
+            board[i] = br.readLine().toCharArray();
         }
-
-        for(int i=0; i<R-8+1; i++) {
-            for(int j=0; j<C-8+1; j++) {
-                String[][] bucket = new String[8][8];
-                for (int k = 0; k < 8; k++) {
-                    for (int l = 0; l < 8; l++) {
-                        bucket[k][l] = map[i+k][j+l];
-                    }
-                }
-
-                answer = Math.min(makeNewMap(bucket), answer);
+        
+        int minPaint = Integer.MAX_VALUE;
+        
+        // 8x8 체스판을 자를 수 있는 모든 위치에서 체크
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                minPaint = Math.min(minPaint, getMinRepaints(board, i, j));
             }
         }
-        System.out.println(answer);
+        
+        System.out.println(minPaint);
     }
-
-    public static int makeNewMap(String[][] bucket) {
-        int a = check(bucket, 0);
-        bucket[0][0] = bucket[0][0].equals("W") ? "B" : "W";
-        int b = check(bucket, 1);
-
-        return Math.min(a, b);
-    }
-
-    public static int check(String[][] bucket, int cnt) {
-        String s = bucket[0][0];
-
-        for(int i=0; i<8; i++) {
-            for(int j=0; j<8; j++) {
-                if((i%2 == 0 && j%2 == 0) || (i%2==1 && j%2==1)) {
-                    if(!bucket[i][j].equals(s)) cnt++;
-                } else {
-                    if(bucket[i][j].equals(s)) cnt++;
+    
+    // 8x8 영역에서 다시 칠해야 할 최소 칸 수를 계산하는 함수
+    public static int getMinRepaints(char[][] board, int startRow, int startCol) {
+        int count1 = 0; // 첫 번째 칸이 'W'로 시작하는 경우
+        int count2 = 0; // 첫 번째 칸이 'B'로 시작하는 경우
+        
+        char[] pattern = {'W', 'B'}; // 두 색의 패턴
+        
+        // 8x8 크기 탐색
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                char expectedChar1 = pattern[(i + j) % 2]; // 첫 칸이 'W'로 시작할 때 예상되는 값
+                char expectedChar2 = pattern[(i + j + 1) % 2]; // 첫 칸이 'B'로 시작할 때 예상되는 값
+                
+                if (board[startRow + i][startCol + j] != expectedChar1) {
+                    count1++;
+                }
+                if (board[startRow + i][startCol + j] != expectedChar2) {
+                    count2++;
                 }
             }
         }
-
-        return cnt;
+        
+        return Math.min(count1, count2); // 두 경우 중 더 적은 값을 반환
     }
 }
