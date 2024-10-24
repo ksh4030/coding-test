5766542ac334af786ae3d00bff812228a33d6e07
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-24 22:44:37 +0900

    [Gold IV] Title: 뱀, Time: 120 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -3,85 +3,103 @@ import java.util.*;
 
 public class Main {
     static String[][] map;
-    static int[] dr = {0, 1, 0, -1};
+    static int[] dr = {0, 1, 0, -1}; // 이동 방향: 동, 남, 서, 북
     static int[] dc = {1, 0, -1, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int answer = 0;
-        int dist = 0;
-        int N = Integer.parseInt(br.readLine());
-        int K = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // 보드 크기
+        int k = Integer.parseInt(br.readLine()); // 사과 개수
+        map = new String[n][n];
 
-        map = new String[N][N];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                map[i][j] = "";
-            }
+        initMap(n); // 맵 초기화
+        placeApples(br, k); // 사과 배치
+
+        int l = Integer.parseInt(br.readLine()); // 방향 전환 횟수
+        Queue<Dir> dirs = readDirections(br, l); // 방향 전환 정보
+
+        System.out.println(simulateSnake(n, dirs)); // 뱀 게임 시뮬레이션 결과 출력
+    }
+
+    static void initMap(int n) {
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(map[i], "");
         }
-        for(int i=0; i<K; i++) {
-            st = new StringTokenizer(br.readLine());
-            map[Integer.parseInt(st.nextToken())-1][Integer.parseInt(st.nextToken())-1] = "apple";
+    }
+
+    static void placeApples(BufferedReader br, int k) throws IOException {
+        for (int i = 0; i < k; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            map[r][c] = "A"; // A는 사과를 의미
         }
+    }
 
-        int L = Integer.parseInt(br.readLine());
-        Queue<Node> q = new LinkedList<>();
-
-        for(int i=0; i<L; i++) {
-            st = new StringTokenizer(br.readLine());
-            q.add(new Node(Integer.parseInt(st.nextToken()), st.nextToken()));
+    static Queue<Dir> readDirections(BufferedReader br, int l) throws IOException {
+        Queue<Dir> dirs = new LinkedList<>();
+        for (int i = 0; i < l; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            String d = st.nextToken();
+            dirs.add(new Dir(t, d));
         }
+        return dirs;
+    }
 
-        map[0][0] = "snake";
-        int r = 0;
-        int c = 0;
-        Deque<Pos> dq = new ArrayDeque<>();
-        dq.add(new Pos(0 , 0));
+    static int simulateSnake(int n, Queue<Dir> dirs) {
+        Deque<Pos> snake = new ArrayDeque<>();
+        snake.add(new Pos(0, 0)); // 뱀 초기 위치
+        map[0][0] = "S"; // S는 뱀을 의미
+
+        int t = 0, r = 0, c = 0, dir = 0; // 시간, 좌표, 방향
         while (true) {
-            answer++;
-            r +=dr[dist];
-            c +=dc[dist];
-            try {
-                if (map[r][c].equals("snake")) {
-                    break;
-                } else if (map[r][c].equals("apple")) {
-                    dq.addLast(new Pos(r, c));
-                    map[r][c] = "snake";
-                } else {
-                    if(!dq.isEmpty()){
-                        Pos pos = dq.pollFirst();
-                        map[pos.r][pos.c] = "";
-                    }
-                    dq.addLast(new Pos(r, c));
-                    map[r][c] = "snake";
-                }
-                if(!q.isEmpty() && answer == q.peek().time) {
-                    String s = q.poll().dist;
-                    if(s.equals("D")) dist = (dist+1)%4;
-                    else if(s.equals("L")) dist = dist - 1 < 0 ? 3 : dist-1;
-                }
-            } catch (Exception e) {
-                break;
+            t++;
+            r += dr[dir];
+            c += dc[dir];
+
+            // 게임 종료 조건
+            if (r < 0 || r >= n || c < 0 || c >= n || map[r][c].equals("S")) {
+                return t;
+            }
+
+            // 사과가 있는 경우
+            if (map[r][c].equals("A")) {
+                snake.addLast(new Pos(r, c));
+                map[r][c] = "S";
+            } else { // 사과가 없는 경우
+                Pos tail = snake.pollFirst();
+                map[tail.r][tail.c] = ""; // 꼬리 이동
+                snake.addLast(new Pos(r, c));
+                map[r][c] = "S";
+            }
+
+            // 방향 전환
+            if (!dirs.isEmpty() && t == dirs.peek().time) {
+                dir = changeDir(dir, dirs.poll().dir);
             }
         }
+    }
 
-        System.out.println(answer);
+    static int changeDir(int dir, String turn) {
+        if (turn.equals("D")) return (dir + 1) % 4; // 오른쪽 회전
+        return (dir + 3) % 4; // 왼쪽 회전
     }
 
     static class Pos {
         int r, c;
-        public Pos(int r, int c) {
+        Pos(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
 
-    static class Node {
+    static class Dir {
         int time;
-        String dist;
-        public Node(int time, String dist) {
+        String dir;
+        Dir(int time, String dir) {
             this.time = time;
-            this.dist = dist;
+            this.dir = dir;
         }
     }
 }
