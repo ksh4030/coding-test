c290bec46cc0be73f542088cbafe430c3c9c7a26
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-26 20:13:54 +0900

    [level 2] Title: 프로세스, Time: 1.31 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,23 +2,31 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for(int n : priorities) {
-            pq.add(n);
-        }
         
-        while (!pq.isEmpty()) {
-            for (int i = 0; i < priorities.length; i++) {
-                if(pq.peek() == priorities[i]) {
-                    pq.poll();
-                    answer++;
-                    if(i == location) {
-                        return answer;
-                    }
-                }
+        Queue<Integer> priority = new LinkedList<>();
+        Queue<Integer> num = new LinkedList<>();
+        
+        for(int i=0; i<priorities.length; i++) {
+            priority.add(priorities[i]);
+            num.add(i);
+        }
+        Arrays.sort(priorities);
+        
+        int idx = priorities.length-1;
+        int cnt = 1;        
+        while(!priority.isEmpty()) {
+            if(priority.peek() == priorities[idx]) {
+                priority.poll();
+                idx--;
+                int n = num.poll();
+                if(n == location) return cnt;
+                else cnt++;
+            } else {
+                priority.add(priority.poll());
+                num.add(num.poll());
             }
         }
+                
         return answer;
     }
 }
