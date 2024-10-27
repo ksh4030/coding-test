9490cadb94a7939e4b054817f181884cd2a2d5e0
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-27 16:36:05 +0900

    [level 3] Title: 이중우선순위큐, Time: 1023.54 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,27 +1,40 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int cnt1 = 0, cnt2 = 0;
         int[] answer = new int[2];
-        for (String s : operations) {
-            String[] arr = s.split(" ");
-            switch(arr[0]){
-                case "I": pq.add(Integer.parseInt(arr[1])); break;
-                case "D": if("-1".equals(arr[1])){
-                    pq.poll();
-                }else{
-                    PriorityQueue<Integer> tpq = new PriorityQueue<>();
-                    int len = pq.size();
-                    for(int i=0;i<len-1;i++) tpq.add(pq.poll());
-                    pq = tpq;
-                    break;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i=0; i<operations.length; i++) {
+            String[] arr = operations[i].split(" ");
+            if(arr[0].equals("I")) {
+                pq.add(Integer.parseInt(arr[1]));
+            } else {
+                if(!pq.isEmpty()) {
+                    if(arr[1].equals("-1")) {
+                        pq.poll();
+                    } else {
+                        PriorityQueue<Integer> bucket = new PriorityQueue<>();
+                        int len = pq.size();
+                        for(int j=0; j<len-1; j++) bucket.add(pq.poll());
+                        pq = bucket;
+                    }
                 }
             }
         }
-
-        if(pq.size()>1) answer[1] = pq.poll();
-        while(pq.size()>0) answer[0] = pq.poll();
+        
+        // System.out.println(pq);
+        
+        if(pq.size() == 1) {            
+            int n = pq.poll();
+            return new int[]{n, n};
+        }
+        if(pq.size() > 1) answer[1] = pq.poll();
+        while(pq.size() > 0) answer[0] = pq.poll();
+        if(answer[1] > answer[0]) {
+            int tmp = answer[1];
+            answer[1] = answer[0];
+            answer[0] = tmp;
+        }
         return answer;
     }
 }
