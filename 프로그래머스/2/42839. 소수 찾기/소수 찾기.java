12e20e44afdddbf98f4197d7e008b13336f9383a
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-28 03:45:13 +0900

    [level 2] Title: 소수 찾기, Time: 24.20 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,58 +1,45 @@
 import java.util.*;
 class Solution {
-    static int answer;
-    static String[] arr;
-    static Map<Integer, Boolean> map;
-    static boolean[] v;
+    static int answer = 0;
+    static HashMap<Integer, Boolean> map = new HashMap<>();
     public int solution(String numbers) {
-        answer = 0;
-        arr = numbers.split("");
-        v = new boolean[arr.length];
-        map = new HashMap<>();
-
-        String s = "";
-        for (int i = 0; i < arr.length; i++) {
-            String[] sel = new String[i+1];
-            per(sel, 0);
+        String[] arr = numbers.split(""); 
+        
+        for(int i=1; i<=numbers.length(); i++) {            
+            per(arr, new String[i], 0, new boolean[arr.length]);
         }
+        
         return answer;
     }
-        public static void per(String[] sel, int idx) {
+    
+    public void per(String[] arr, String[] sel, int idx, boolean[] v) {
         if(idx == sel.length) {
             String s = "";
-            for (String value : sel) {
-                s += value;
-            }
+            for(String a : sel) s+=a;
             int n = Integer.parseInt(s);
+            
             if(map.get(n) == null) {
-                prime(n);
+                if(isPrime(n)) answer++;
                 map.put(n, true);
-            }
+            }            
             return;
         }
-
-        for (int i = 0; i < arr.length; i++) {
+        
+        for(int i=0; i<arr.length; i++) {
             if(!v[i]) {
                 v[i] = true;
                 sel[idx] = arr[i];
-                per(sel, idx+1);
+                per(arr, sel, idx+1, v);
                 v[i] = false;
             }
         }
     }
-
-    public static void prime(int n) {
-        if(n < 2) return;
-        if(n == 2) {
-            answer++;
-            return;
+    
+    public boolean isPrime(int n) {     
+        if(n < 2) return false;
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(n%i == 0) return false;
         }
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0) {
-                return;
-            }
-        }
-        answer++;
+        return true;
     }
 }
