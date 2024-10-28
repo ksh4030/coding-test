809fc313bb7eefb11b5052c1efe88fe84c8bb9ec
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-28 22:18:09 +0900

    [level 3] Title: 단어 변환, Time: 14.33 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,39 @@
 import java.util.*;
 class Solution {
-    public int solution(String begin, String target, String[] words) {
+    public int solution(String begin, String target, String[] words) {        
         int answer = 0;
-        if (Arrays.asList(words).contains(target)) {
-            answer = bfs(begin, target, words);
-        }        
+        if(Arrays.asList(words).contains(target)) answer = bfs(begin, target, words);
         return answer;
     }
     
-    public static int bfs(String begin, String target, String[] words) {
+    public int bfs(String begin, String target, String[] words) {
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(begin, 0));
-
-        while (!q.isEmpty()) {
+        
+        while(!q.isEmpty()) {
             Node cur = q.poll();
-            if(cur.s.equals(target)) {
-                return cur.cnt;
-            }
-
-            for (int i = 0; i < words.length; i++) {
+            if(target.equals(cur.s)) return cur.cnt;
+            
+            for(int i=0; i<words.length; i++) {
                 if(diff(cur.s, words[i])) {
                     q.add(new Node(words[i], cur.cnt+1));
                 }
             }
         }
+        
         return 0;
     }
-
-    public static boolean diff(String a, String b) {
-        String[] arr1 = a.split("");
-        String[] arr2 = b.split("");
+    
+    public boolean diff(String s, String word) {
         int cnt = 0;
-
-        for (int i = 0; i < arr1.length; i++) {
-            if(!arr1[i].equals(arr2[i])) cnt++;
+        for(int i=0; i<word.length(); i++) {
+            if(s.charAt(i) != word.charAt(i)) cnt++;
+            if(cnt > 1) return false;
         }
-
-        if(cnt == 1) return true;
-        else return false;
+        return true;
     }
-
-    static class Node {
+    
+    class Node {
         String s;
         int cnt;
         public Node(String s, int cnt) {
