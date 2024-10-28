ac22532ff2859256424fb2541f89bcbe3b54386b
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-28 21:59:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.77 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,56 +1,41 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {0,1,0,-1};
-    static int[] dc = {1,0,-1,0};
-    static Queue<Node> q;
-    static int targetR, targetC, answer;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, -1, 0, 1};
     static boolean[][] v;
+    
     public int solution(int[][] maps) {
-        answer = Integer.MAX_VALUE;
         v = new boolean[maps.length][maps[0].length];
-
-        targetR = maps.length-1;
-        targetC = maps[0].length-1;
-
-        q = new LinkedList<>();
-        bfs(maps);
-        if(answer == Integer.MAX_VALUE) {
-            answer = -1;
-        }
+        int answer = bfs(maps);        
         return answer;
     }
     
-    public static void bfs(int[][] maps) {
+    public int bfs(int[][] maps) {
+        Queue<Node> q = new LinkedList<>();
         q.add(new Node(0,0,1));
-
-        while (!q.isEmpty()) {
+        v[0][0] = true;
+        
+        while(!q.isEmpty()) {
             Node cur = q.poll();
-
-            if (cur.r == targetR && cur.c == targetC) {
-                answer = Math.min(answer, cur.cnt);
+            if(cur.r == maps.length-1 && cur.c == maps[0].length-1) {
+                return cur.cnt;
             }
-
-            for (int i = 0; i < 4; i++) {
+            
+            for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
-
-                try {
-                    if(maps[nr][nc]==1 && !v[nr][nc]) {
-                        q.add(new Node(nr, nc, cur.cnt+1));
-                        v[nr][nc] = true;
-                    }
-                } catch (Exception e) {
-                    continue;
-                }
-
+                
+                if(nr<0 || nc<0 || nr>=maps.length || nc>=maps[0].length || maps[nr][nc]==0 || v[nr][nc]) continue;
+                q.add(new Node(nr, nc, cur.cnt+1));
+                v[nr][nc] = true;
             }
         }
+        
+        return -1;
     }
-
-    public static class Node {
-        int r;
-        int c;
-        int cnt;
+    
+    class Node {
+        int r, c, cnt;
         public Node(int r, int c, int cnt) {
             this.r = r;
             this.c = c;
