7aac03fc8a308880ebe62b37dd84e546d69c9b11
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-29 16:58:10 +0900

    [level 3] Title: 입국심사, Time: 67.39 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,29 +1,25 @@
 import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        int size = times.length;
-        Arrays.sort(times);
         
         long left = 0;
-        long right = times[times.length - 1] * (long)n;
+        long right = times[times.length-1] * (long)n;
         long mid = 0;
-        long target = 0;
+        long sum = 0;
         
-        while(left <= right) {
-            mid = (left+right) / 2;
-            target = 0;
-            for(int i=0; i<times.length; i++) {
-                target += (mid / times[i]);
-            }
-            if(target < n) {
-                left = mid + 1;
-            } else {
+        while(left<=right) {
+            mid = (right + left) / 2;
+            sum = 0;
+            for(int i=0; i<times.length; i++) sum += mid/times[i];
+            if(sum >= n) {
                 right = mid - 1;
                 answer = mid;
+            } else {                
+                left = mid + 1;
             }
         }
+        
         return answer;
     }
 }
