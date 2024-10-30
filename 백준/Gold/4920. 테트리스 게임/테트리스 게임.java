0c6b21c1ff9a24af06c2244d6f9c985e48f2568b
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-10-30 21:47:15 +0900

    [Gold IV] Title: 테트리스 게임, Time: 256 ms, Memory: 20868 KB -BaekjoonHub

--- 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
+++ 백준/Gold/4920. 테트리스 게임/테트리스 게임.java
@@ -3,78 +3,78 @@ import java.util.*;
 
 public class Main {
 
-    static int[][][] figure = {
-        {
-            {0, 0, 0, 0}, {0, 1, 2, 3}
-        },
-        {
-            {0, 1, 2, 3}, {0, 0, 0, 0}
-        },
+    static int[][][] standard = {
+            {
+                    {0, 0, 0, 0}, {0, 1, 2, 3}
+            },
+            {
+                    {0, 1, 2, 3}, {0, 0, 0, 0}
+            },
 
-        {
-            {0, 0, 1, 1}, {0, 1, 1, 2}
-        },
-        {
-            {0, 1, 1, 2}, {0, 0, -1, -1}
-        },
+            {
+                    {0, 0, 1, 1}, {0, 1, 1, 2}
+            },
+            {
+                    {0, 1, 1, 2}, {0, 0, -1, -1}
+            },
 
-        {
-            {0, 0, 0, 1}, {0, 1, 2, 2}
-        },
-        {
-            {0, 1, 2, 2}, {0, 0, 0, -1}
-        },
-        {
-            {0, 1, 1, 1}, {0, 0, 1, 2}
-        },
-        {
-            {0, 0, 1, 2}, {0, 1, 0, 0}
-        },
+            {
+                    {0, 0, 0, 1}, {0, 1, 2, 2}
+            },
+            {
+                    {0, 1, 2, 2}, {0, 0, 0, -1}
+            },
+            {
+                    {0, 1, 1, 1}, {0, 0, 1, 2}
+            },
+            {
+                    {0, 0, 1, 2}, {0, 1, 0, 0}
+            },
 
-        {
-            {0, 1, 1, 1}, {0, -1, 0, 1}
-        },
-        {
-            {0, 1, 1, 2}, {0, 0, 1, 0}
-        },
-        {
-            {0, 0, 0, 1}, {0, 1, 2, 1}
-        },
-        {
-            {0, 1, 1, 2}, {0, -1, 0, 0}
-        },
+            {
+                    {0, 1, 1, 1}, {0, -1, 0, 1}
+            },
+            {
+                    {0, 1, 1, 2}, {0, 0, 1, 0}
+            },
+            {
+                    {0, 0, 0, 1}, {0, 1, 2, 1}
+            },
+            {
+                    {0, 1, 1, 2}, {0, -1, 0, 0}
+            },
 
-        {
-            {0, 0, 1, 1}, {0, 1, 1, 0}
-        },
+            {
+                    {0, 0, 1, 1}, {0, 1, 1, 0}
+            },
 
     };
-    static int[][] map;
+    static int[][] board;
     static int N;
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int input = 1;
+        int idx = 1;
         while ((N = Integer.parseInt(br.readLine().trim())) != 0) {
-            map = new int[N][N];
+            board = new int[N][N];
             int max = Integer.MIN_VALUE;
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine().trim());
                 for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    board[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    for (int a = 0; a < figure.length; a++) {
+                    for (int a = 0; a < standard.length; a++) {
                         int sum = 0;
                         boolean flag = true;
                         for (int b = 0; b < 4; b++) {
-                            int nx = i + figure[a][0][b];
-                            int ny = j + figure[a][1][b];
+                            int nx = i + standard[a][0][b];
+                            int ny = j + standard[a][1][b];
                             if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                                sum += map[nx][ny];
+                                sum += board[nx][ny];
                             } else {
                                 flag = false;
                                 break;
@@ -86,8 +86,8 @@ public class Main {
                     }
                 }
             }
-            System.out.println(input + ". " + max);
-            input++;
+            System.out.println(idx + ". " + max);
+            idx++;
         }
     }
 }
