e76916283c2d016cd48c7667c345a4dc759833f1
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-13 20:23:17 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 2.53 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,39 +1,26 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] mats, String[][] park) {
         int answer = -1;
-        Arrays.sort(mats);
+        int maxSize = 0;
+        int[][] dp = new int[park.length][park[0].length];
         
-        for(int n=mats.length-1; n>=0; n--) {
-            
-            for(int i=0; i<park.length; i++) {
-                for(int j=0; j<park[0].length; j++) {
-                    if(!park[i][j].equals("-1")) continue;
-                    boolean flag = true;
-                    
-                    loop:
-                    for(int l = i; l<i+mats[n]; l++) {
-                        for(int m=j; m<j+mats[n]; m++) {
-                            try{
-                                if(!park[l][m].equals("-1")) {
-                                    flag = false;
-                                    break loop;
-                                }
-                            } catch (Exception e) {
-                                flag = false;
-                                break loop;
-                            }
-                        }
+        for(int i=0; i<park.length; i++) {
+            for(int j=0; j<park[0].length; j++) {
+                if(park[i][j].equals("-1")) {
+                    if(i==0 || j==0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]) + 1;
                     }
-                    
-                    if(flag) {
-                        return mats[n];
-                    }
-                    
+                    maxSize = Math.max(maxSize, dp[i][j]);
                 }
             }
-            
+        }
+        
+        Arrays.sort(mats);
+        for(int i=mats.length-1; i>=0; i--) {
+            if(mats[i] <= maxSize) return mats[i];
         }
         
         return answer;
