0e63ee7286eb47f6ec87767f0808c4afcfc43012
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-17 16:55:10 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 12.57 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,25 +1,43 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {0, sequence.length-1};
-        int head = 0;
-        int tail = 1;
-        int sum = sequence[0];        
+        int[] answer = new int[2];
         
-        while(head < tail) {
-            if(sum == k){
-                if(tail-head-1 < answer[1]-answer[0]) {
-                    answer[0] = head;
-                    answer[1] = tail-1;
-                }                
-                sum -= sequence[head++];
-            } else if (sum > k) {
-                sum -= sequence[head++];
-            } else if (tail < sequence.length) {
-                sum += sequence[tail++];
-            } else {
+        if(k == sequence[0]) return new int[]{0,0};
+        
+        int start = 0;
+        int end = 1;
+        
+        int sum = sequence[0] + sequence[1];
+        int curSize = 2;
+        int minSize = Integer.MAX_VALUE;
+        
+        while(true) {
+            if(start > end) break;
+            if(end > sequence.length) break;
+            try {
+                if(sum < k) {
+                     end++;
+                     curSize++;
+                     sum += sequence[end];
+                 } else if (sum > k) {
+                     sum -= sequence[start];
+                     start++;
+                     curSize--;
+                 } else {
+                    if(minSize > curSize) {
+                        answer[0] = start;
+                        answer[1] = end;
+                        minSize = curSize;
+                    }
+                    sum -= sequence[start];
+                    start++;
+                    curSize--;                    
+                 }
+            } catch (Exception e) {
                 break;
             }
+            
         }
         
         return answer;
