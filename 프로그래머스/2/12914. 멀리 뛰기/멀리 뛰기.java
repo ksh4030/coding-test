bad6b6a6f531f884623a13f3b39b2c35c9b892fe
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-17 16:38:37 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.13 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,19 +1,20 @@
 import java.util.*;
 class Solution {
-    static final long NUMBER = 1234567;
+    static final int MOD = 1234567;
     public long solution(int n) {
         long answer = 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
         long num1 = 1;
         long num2 = 2;
         
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        
         for(int i=3; i<=n; i++) {
-            long tmp = num2;
-            num2 = (num1+num2)%NUMBER;
-            num1 = tmp;            
+            answer = (num1 + num2) % MOD;            
+            num1 = num2;
+            num2 = answer;
         }
         
-        return num2%NUMBER;
+        return answer;
     }
 }
