bc40aa2f3552d682a818286f8f630b3017c48198
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-19 17:34:07 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.25 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,50 +1,44 @@
 import java.util.*;
 class Solution {
-    public char[][] map;
-    public int win(char c) {
-        int game = 0;
-        for(int i = 0; i < 3; i++)
-        {
-            if(map[i][0] == c && map[i][0] == map[i][1] && map[i][1] == map[i][2])
-                game++;
-            if(map[0][i] == c && map[0][i] == map[1][i] && map[1][i] == map[2][i])
-                game++;
-        }
-        if(map[0][0] == c && map[0][0] == map[1][1] && map[1][1] == map[2][2])
-            game++;
-        if(map[0][2] == c && map[0][2] == map[1][1] && map[1][1] == map[2][0])
-            game++;
-        return game;
+    static String[][] map = new String[3][3];
+    public int solution(String[] board) {        
+        for(int i=0; i<board.length; i++) map[i] = board[i].split("");        
+        return isPossible() ? 1 : 0;
     }
-    public int solution(String[] board) {
-        int answer = 1;
-        map = new char[3][3];
-        int oCnt = 0;
-        int xCnt = 0;
-        for(int i = 0; i < 3; i++)
-        {
-            for(int j = 0; j < 3; j++)
-            {
-                map[i][j] = board[i].charAt(j);
-                if(map[i][j] == 'O')
-                    oCnt++;
-                if(map[i][j] == 'X')
-                    xCnt++;
+    
+    public boolean isPossible() {
+        int cntO = 0, cntX = 0;
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(map[i][j].equals("O")) cntO++;
+                if(map[i][j].equals("X")) cntX++;
             }
         }
-        if(xCnt > oCnt || oCnt - xCnt > 1)
-            return 0;
-        if(win('O') > 0 && win('X') > 0)
-            return 0;
-        if(win('O') > 0) {
-            if(oCnt == xCnt)//o가 이겼으면 o가 한개 많아야함
-                return 0;
-        }
-        if(win('X') > 0) {
-            if(oCnt > xCnt)
-                return 0;
+        boolean correctO = correct("O");
+        boolean correctX = correct("X");        
+        
+        if(cntX > cntO) return false;
+        if(Math.abs(cntX - cntO) >= 2) return false;
+        if(correctO && correctX) return false;
+        if(correctX && (cntO > cntX)) return false;
+        if(correctO && (cntO == cntX)) return false;
+        
+        return true;
+    }
+    
+    public boolean correct(String target) {
+        for(int i=0; i<3; i++) {
+            if(map[i][0].equals(target)) {
+                if(map[i][1].equals(target) && (map[i][2]).equals(target)) return true;
+            }
+            if(map[0][i].equals(target)) {
+                if(map[1][i].equals(target) && (map[2][i]).equals(target)) return true;
+            }            
         }
         
-        return answer;
+        if(map[0][0].equals(target) && map[1][1].equals(target) && map[2][2].equals(target)) return true;
+        if(map[0][2].equals(target) && map[1][1].equals(target) && map[2][0].equals(target)) return true;
+        
+        return false;
     }
 }
