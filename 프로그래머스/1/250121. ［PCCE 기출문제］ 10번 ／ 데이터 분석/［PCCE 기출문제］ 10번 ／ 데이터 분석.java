cc30455abedc74d9de42c4f2d185bf132e3a4da1
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-22 01:26:39 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.79 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,17 +1,31 @@
 import java.util.*;
-
 class Solution {
-    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {        
+        List<int[]> list = new ArrayList<>();
         
-        Map<String, Integer> colOrder = new HashMap<>();
-        colOrder.put("code", 0);
-        colOrder.put("date", 1);
-        colOrder.put("maximum", 2);
-        colOrder.put("remain", 3);
+        int target = -1;
+        if(ext.equals("code")) target = 0;
+        if(ext.equals("date")) target = 1;
+        if(ext.equals("maximum")) target = 2;
+        if(ext.equals("remain")) target = 3;
         
-        int[][] filteredData = Arrays.stream(data).filter(x -> x[colOrder.get(ext)] < val_ext).toArray(int[][]::new);
-        Arrays.sort(filteredData, (o1, o2) ->  o1[colOrder.get(sort_by)] - o2[colOrder.get(sort_by)]);
-
-        return filteredData;
+        for(int i=0; i<data.length; i++) {
+            if(data[i][target] < val_ext) list.add(data[i]);
+        }
+        
+        Collections.sort(list, (o1, o2) -> {
+            if(sort_by.equals("code")) return o1[0] - o2[0];
+            if(sort_by.equals("date")) return o1[1] - o2[1];
+            if(sort_by.equals("maximum")) return o1[2] - o2[2];
+            if(sort_by.equals("remain")) return o1[3] - o2[3];
+            return o1[0] - o2[0];
+        });
+        
+        int[][] answer = new int[list.size()][4];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }
