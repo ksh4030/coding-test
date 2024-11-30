ad253f700742a4eed55d5121c061edccd9d379d5
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-11-30 16:59:34 +0900

    [level 2] Title: 미로 탈출, Time: 15.07 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,68 +1,69 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, -1, 0, 1};
-    static boolean[][] v;
-    static Node start, bucket;
+    static int[] dr = {-1,1,0,0};
+    static int[] dc = {0,0,-1,1};
     static String[][] map;
+    static boolean[][] v;
     public int solution(String[] maps) {
         int answer = 0;
-        v = new boolean[maps.length][maps[0].length()];        
-        map = new String[maps.length][maps[0].length()];
-        
-        for(int i=0; i<maps.length; i++) {
-            map[i] = maps[i].split("");
-        }
-        
-        for(int i=0; i<maps.length; i++) {
-            for(int j=0; j<maps[0].length(); j++) {
+        int sum = 0;
+        makeMap(maps);
+                
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[0].length; j++) {
                 if(map[i][j].equals("S")) {
-                    start = new Node(i,j,0);
+                    v = new boolean[maps.length][maps[0].length()];
+                    v[i][j] = true;
+                    sum = bfs(new Pos(i,j,0), "L");
+                    if(sum == -1) return -1;
+                    else answer = sum;
+                    break;
+                }
+            }
+        }
+        sum = 0;
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[0].length; j++) {
+                if(map[i][j].equals("L")) {
+                    v = new boolean[maps.length][maps[0].length()];
+                    v[i][j] = true;
+                    sum = bfs(new Pos(i,j,0), "E");
+                    if(sum == -1) return -1;
                     break;
                 }
             }
         }
         
-        int n = bfs(start, "L");
-        if(n == -1) return -1;
-        answer += n;
-        
-        v = new boolean[maps.length][maps[0].length()];
-        n = bfs(bucket, "E");
-        if(n == -1) return -1;
-        answer += n;
-        
-        return answer;
+        return answer + sum;
     }
     
-    public static int bfs(Node node, String target) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(node);
-        v[node.r][node.c] = true;
+    public int bfs(Pos pos, String target) {
+        Queue<Pos> q = new LinkedList<>();
+        q.add(pos);
         
         while(!q.isEmpty()) {
-            Node cur = q.poll();
-            
-            if(map[cur.r][cur.c].equals(target)) {
-                if(target.equals("L")) bucket = new Node(cur.r, cur.c, 0);
-                return cur.cnt;
-            }      
+            Pos cur = q.poll();
+            if(map[cur.r][cur.c].equals(target))return cur.cnt;
             
             for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
-                
-                if(nr<0 || nc<0 || nr>=v.length || nc>=v[0].length || v[nr][nc] || map[nr][nc].equals("X")) continue;
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || v[nr][nc] || map[nr][nc].equals("X")) continue;
                 v[nr][nc] = true;
-                q.add(new Node(nr, nc, cur.cnt+1));
+                q.add(new Pos(nr, nc, cur.cnt+1));
             }
-        }        
+        }
         return -1;
     }
     
-    static class Node {
+    public void makeMap(String[] maps) {
+        map = new String[maps.length][maps[0].length()];
+        for(int i=0; i<maps.length; i++) map[i] = maps[i].split("");
+    }
+    
+    class Pos {
         int r, c, cnt;
-        Node(int r, int c, int cnt) {
+        public Pos(int r, int c, int cnt) {
             this.r = r;
             this.c = c;
             this.cnt = cnt;
