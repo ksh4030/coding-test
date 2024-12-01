e32eb0825d1d7e36babd6130f848a2e4eb3e6231
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-12-01 17:06:17 +0900

    [level 3] Title: 최고의 집합, Time: 1.66 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -2,15 +2,10 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int s) {
         int[] answer = new int[n];
-        if (n > s) return new int[]{-1};        
-        
-        for (int i = 0; i < n; i++) {
-            answer[i] = s/n;
-        }
-        
-        for (int i = 0; i < s%n; i++) {
-            answer[i]++;
-        }
+        if (n > s) return new int[]{-1};
+                
+        for (int i = 0; i < n; i++) answer[i] = s/n;
+        for (int i = 0; i < s%n; i++) answer[i]++;
         
         Arrays.sort(answer);
         return answer;
