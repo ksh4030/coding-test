6074decdb8a31d1a6db99f51d306dcee65c2e218
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-12-07 16:48:05 +0900

    [level 3] Title: 가장 먼 노드, Time: 57.28 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,37 +1,30 @@
 import java.util.*;
-
 class Solution {
-    static List<List<Integer>> graph = new ArrayList<>();
-    static boolean[] visit;
-    
+    List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] v;
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        visit = new boolean[n+1];
+        v = new boolean[n+1];
         
-        for(int i=0; i<=n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
         
         for(int[] arr : edge) {
             graph.get(arr[0]).add(arr[1]);
             graph.get(arr[1]).add(arr[0]);
         }
         
-        answer = bfs();
-        
-        return answer;
+        return bfs();
     }
     
-    public static int bfs() {
+    public int bfs() {
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(1, 0));
-        visit[1] = true;
+        v[1] = true;
         int max = 0;
         int ans = 0;
         
         while(!q.isEmpty()) {
             Node cur = q.poll();
-            
             if(cur.cnt == max) ans++;
             if(cur.cnt > max) {
                 ans = 1;
@@ -39,20 +32,22 @@ class Solution {
             }
             
             for(int i=0; i<graph.get(cur.num).size(); i++) {
-                if(!visit[graph.get(cur.num).get(i)]){
-                    q.add(new Node(graph.get(cur.num).get(i), cur.cnt + 1));
-                    visit[graph.get(cur.num).get(i)] = true;
+                int next = graph.get(cur.num).get(i);
+                if(!v[next]) {
+                    v[next] = true;
+                    q.add(new Node(next, cur.cnt+1));
                 }
             }
         }
+        
         return ans;
     }
     
-    static class Node {
+    class Node {
         int num, cnt;
-        public Node(int num, int cnt){
+        public Node(int num, int cnt) {
             this.num = num;
-            this.cnt = cnt;            
+            this.cnt = cnt;
         }
     }
 }
