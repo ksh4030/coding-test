97d5925d8e5654bf3bea516d2fe05812c81ee3ac
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2024-12-09 16:14:56 +0900

    [level 3] Title: 여행경로, Time: 73.01 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,23 +3,24 @@ class Solution {
     static List<String> list = new ArrayList<>();
     static boolean[] v;
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
+        v = new boolean[tickets.length];
         
-        v = new boolean[tickets.length];        
-        bfs(tickets, "ICN", "ICN", 0);
+        dfs(tickets, "ICN", "ICN", 0);
+        
         Collections.sort(list);
         return list.get(0).split(" ");
     }
     
-    public void bfs(String[][] tickets, String cur, String ans, int depth) {
+    public void dfs(String[][] tickets, String cur, String ans, int depth) {
         if(depth == tickets.length) {
             list.add(ans);
             return;
         }
+        
         for(int i=0; i<tickets.length; i++) {
             if(!v[i] && cur.equals(tickets[i][0])) {
                 v[i] = true;
-                bfs(tickets, tickets[i][1], ans + " " + tickets[i][1], depth+1);
+                dfs(tickets, tickets[i][1], ans+" "+tickets[i][1], depth+1);
                 v[i] = false;
             }
         }
