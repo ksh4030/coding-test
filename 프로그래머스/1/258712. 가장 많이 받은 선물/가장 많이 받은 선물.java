d977a7c81d016698d1bd67ac62090b88ee5f920d
ksh4030 <124498333+ksh4030@users.noreply.github.com>
2025-01-03 23:54:31 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 203.93 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,47 +1,66 @@
 import java.util.*;
 
 class Solution {
+    static Map<String, List<String>> give = new HashMap<>();
+    static Map<String, List<String>> receive = new HashMap<>();
+    
     public int solution(String[] friends, String[] gifts) {
         int answer = 0;
-        Map<String, Integer> map = new HashMap<>();
-        int[][] board = new int[friends.length][friends.length];
-        int[] score = new int[friends.length];
-        int[] ans = new int[friends.length];
-        
-        for(int i=0; i<friends.length; i++) map.put(friends[i], i);        
-        for(int i=0; i<gifts.length; i++) {
-            String[] bucket = gifts[i].split(" ");
-            board[map.get(bucket[0])][map.get(bucket[1])] += 1;
+        initMap(friends, gifts);
+        int[] res = predict(friends);
+        return res[res.length-1];
+    }
+    
+    public void initMap(String[] friends, String[] gifts) {
+        for(String s : friends) {
+            give.put(s, new LinkedList<>());
+            receive.put(s, new LinkedList<>());
         }
         
-        for(int i=0; i<friends.length; i++) {
-            int r = 0;
-            int c = 0;
-            for(int j=0; j<friends.length; j++) {
-                r += board[i][j];
-                c += board[j][i];
-            }            
-            score[i] = r-c;
+        for(String s : gifts) {
+            String[] arr = s.split(" ");
+            give.get(arr[0]).add(arr[1]);
+            receive.get(arr[1]).add(arr[0]);
         }
+    }
+    
+    public int[] predict(String[] friends) {
+        int[] res = new int[friends.length];
         
-        for(int i=0; i<friends.length; i++) {
+        for(int i=0; i<friends.length-1; i++) {
             for(int j=i+1; j<friends.length; j++) {
-                if(board[i][j] > board[j][i]){
-                    ans[i]++;
-                } else if(board[i][j] < board[j][i]) {
-                    ans[j]++;
+                String a = friends[i];
+                String b = friends[j];
+                
+                int aNum = getGiveCnt(a, b);
+                int bNum = getGiveCnt(b, a);
+                
+                if(aNum > bNum) {
+                    res[i]++;
+                } else if (bNum > aNum) {
+                    res[j]++;
                 } else {
-                    if(score[i] > score[j]) {
-                        ans[i]++;
-                    } else if(score[i] < score[j]) {
-                        ans[j]++;
+                    int aGiftIdx = give.get(a).size() - receive.get(a).size();
+                    int bGiftIdx = give.get(b).size() - receive.get(b).size();
+                    
+                    if(aGiftIdx > bGiftIdx) {
+                        res[i]++;
+                    } else if (bGiftIdx > aGiftIdx) {
+                        res[j]++;
                     }
                 }
             }
         }
-        // System.out.println(Arrays.deepToString(board));
-        // System.out.println(Arrays.toString(ans));
-        answer = Arrays.stream(ans).max().orElseThrow();
-        return answer;
+        
+        Arrays.sort(res);
+        return res.clone();
+    }
+    
+    public int getGiveCnt(String giveMan, String receiveMan) {
+        int cnt = 0;
+        for(int i=0; i<give.get(giveMan).size(); i++) {
+            if(give.get(giveMan).get(i).equals(receiveMan)) cnt++;
+        }
+        return cnt;
     }
 }
