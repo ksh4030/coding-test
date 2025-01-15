522ff612efb3c025ed04742ef45f7e26c2895629
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-01-15 15:52:00 +0900

    [level 2] Title: 땅따먹기, Time: 15.20 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,17 +1,27 @@
 class Solution {
     int solution(int[][] land) {
+        int n = land.length;
+        
+        // 첫 번째 행부터 시작하여 점수를 갱신해 나갑니다.
+        for (int i = 1; i < n; i++) {
+            // 이전 행의 각 열을 기준으로, 같은 열을 제외한 최대값을 더해줍니다.
+            for (int j = 0; j < 4; j++) {
+                int maxPrev = 0;
+                for (int k = 0; k < 4; k++) {
+                    if (j != k) {
+                        maxPrev = Math.max(maxPrev, land[i - 1][k]);
+                    }
+                }
+                land[i][j] += maxPrev;
+            }
+        }
+        
+        // 마지막 행에서의 최대값을 구합니다.
         int answer = 0;
-        for (int i = 1; i < land.length; i++) {
-            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
-            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
-            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
-            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        for (int j = 0; j < 4; j++) {
+            answer = Math.max(answer, land[n - 1][j]);
         }
-
-        for (int i = 0; i < 4; i++) {
-            answer = Math.max(answer, land[land.length-1][i]);
-        }
-
+        
         return answer;
     }
 }
