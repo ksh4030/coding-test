8aca4671820f83de70adffde04620e990ea583f4
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-01-18 21:13:27 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 144 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -12,18 +12,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         init();
 
-        for (int i = 0; i < cmd.length; i++) {
+        for(int i=0; i<cmd.length; i++) {
             switch (cmd[i]) {
-                case 1: east(); break;
-                case 2: west(); break;
-                case 3: north(); break;
-                case 4: south(); break;
+                case 1:
+                    east();
+                    break;
+                case 2:
+                    west();
+                    break;
+                case 3:
+                    north();
+                    break;
+                case 4:
+                    south();
+                    break;
             }
         }
     }
 
     public static void check() {
-        if (map[r][c] == 0) {
+        if(map[r][c] == 0) {
             map[r][c] = dice[3][1];
         } else {
             dice[3][1] = map[r][c];
@@ -32,21 +40,20 @@ public class Main {
     }
 
     public static void east() {
-        if (c + 1 >= map[0].length) return;
-
+        if(r<0 || c+1<0 || r>=map.length || c+1>=map[0].length) return;
         int tmp = dice[1][1];
         dice[1][1] = dice[1][0];
         dice[1][0] = dice[3][1];
         dice[3][1] = dice[1][2];
         dice[1][2] = tmp;
 
-        c += 1;
+        c+=1;
         check();
         System.out.println(dice[1][1]);
     }
 
     public static void west() {
-        if (c - 1 < 0) return;
+        if(r<0 || c-1<0 || r>=map.length || c-1>=map[0].length) return;
 
         int tmp = dice[1][1];
         dice[1][1] = dice[1][2];
@@ -54,13 +61,13 @@ public class Main {
         dice[3][1] = dice[1][0];
         dice[1][0] = tmp;
 
-        c -= 1;
+        c-=1;
         check();
         System.out.println(dice[1][1]);
     }
 
     public static void north() {
-        if (r - 1 < 0) return;
+        if(r-1<0 || c<0 || r-1>=map.length || c>=map[0].length) return;
 
         int tmp = dice[1][1];
         dice[1][1] = dice[2][1];
@@ -68,13 +75,13 @@ public class Main {
         dice[3][1] = dice[0][1];
         dice[0][1] = tmp;
 
-        r -= 1;
+        r-=1;
         check();
         System.out.println(dice[1][1]);
     }
 
     public static void south() {
-        if (r + 1 >= map.length) return;
+        if(r+1<0 || c<0 || r+1>=map.length || c>=map[0].length) return;
 
         int tmp = dice[1][1];
         dice[1][1] = dice[0][1];
@@ -82,7 +89,7 @@ public class Main {
         dice[3][1] = dice[2][1];
         dice[2][1] = tmp;
 
-        r += 1;
+        r+=1;
         check();
         System.out.println(dice[1][1]);
     }
@@ -100,15 +107,14 @@ public class Main {
         map = new int[N][M];
         cmd = new int[cntCmd];
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < cntCmd; i++) {
+        for(int i=0; i<cntCmd; i++) {
             cmd[i] = Integer.parseInt(st.nextToken());
         }
     }
