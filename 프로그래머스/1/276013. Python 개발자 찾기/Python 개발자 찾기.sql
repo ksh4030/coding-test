c43a97c34dd27f1a6a6d7efc91c83a7e414a8515
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-01-24 17:51:21 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
 select id, email, first_name, last_name
 from developer_infos
-where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python'
+where 'Python' in (skill_1, skill_2, skill_3)
 order by id;
