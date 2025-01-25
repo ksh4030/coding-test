e2f73d0ee68866337362e2b1ea41f4648ad3022b
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-01-25 21:13:06 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,5 +1,4 @@
 -- 코드를 입력하세요
-SELECT animal_type, ifnull(name, 'No name') as name, sex_upon_intake
+SELECT animal_type, ifnull(name, 'No name'), sex_upon_intake
 from animal_ins
-order by animal_id
-;
+order by animal_id;
