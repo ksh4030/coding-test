e766e8d9a9eaa1f2ebc116d3b1a945c4260aace4
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-01-28 01:10:04 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,9 +1,6 @@
--- 코드를 작성해주세요
 select sum(b.score) as score, a.emp_no, a.emp_name, a.position, a.email
-from hr_employees a
-join hr_grade b on a.emp_no = b.emp_no
-where b.year = '2022'
+from hr_employees a, hr_grade b
+where a.emp_no = b.emp_no
 group by a.emp_no
 order by score desc
-limit 1
-;
+limit 1;
