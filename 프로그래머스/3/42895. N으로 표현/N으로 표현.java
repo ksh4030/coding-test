cb44062781a3763b92a0047e44d02c6f4af8801b
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-06 16:45:50 +0900

    [level 3] Title: N으로 표현, Time: 23.90 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,37 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        if (N == number) {
-            return 1;
+        int answer = 0;
+        if(N == number) return 1;
+        return find(N, number);
+    }
+    
+    public int find(int N, int number) {
+        List<Set<Integer>> list = new ArrayList<>();
+        for(int i=0; i<=8; i++) {
+            list.add(new HashSet<>());
         }
-        
-        List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<>());
-        }
+        list.get(1).add(N);
 
-        dp.get(1).add(N);
-
-        for (int i = 2; i <= 8; i++) {
-            int baseNum = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp.get(i).add(baseNum);
-
-            for (int j = 1; j < i; j++) {
-                for (int num1 : dp.get(j)) {
-                    for (int num2 : dp.get(i - j)) {
-                        dp.get(i).add(num1 + num2);
-                        dp.get(i).add(num1 - num2);
-                        dp.get(i).add(num1 * num2);
-                        if (num2 != 0) {
-                            dp.get(i).add(num1 / num2);
+        for(int i=2; i<=8; i++) {
+            int curNum = Integer.parseInt(String.valueOf(N).repeat(i));
+            list.get(i).add(curNum);
+            for(int j=1; j<i; j++) {
+                for(int num1 : list.get(j)) {
+                    for(int num2 : list.get(i-j)) {
+                        list.get(i).add(num1 + num2);
+                        list.get(i).add(num1 - num2);
+                        list.get(i).add(num1 * num2);
+                        if(num2 != 0) {
+                            list.get(i).add(num1 / num2);
                         }
                     }
                 }
             }
-            
-            if (dp.get(i).contains(number)) {
-                return i;
-            }
+            if(list.get(i).contains(number)) return i;
         }
         return -1;
     }
