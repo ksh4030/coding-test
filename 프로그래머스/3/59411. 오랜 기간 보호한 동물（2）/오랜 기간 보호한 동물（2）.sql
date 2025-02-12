e9735c5fa2fed07d8b054fbc17ad8a9e1c2a5ee8
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-12 22:42:36 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT a.animal_id, a.name
-from animal_ins a
-join animal_outs b on a.animal_id = b.animal_id
+select a.animal_id, a.name
+from animal_ins a, animal_outs b
+where a.animal_id = b.animal_id
 order by b.datetime - a.datetime desc
-limit 2;
+limit 2
