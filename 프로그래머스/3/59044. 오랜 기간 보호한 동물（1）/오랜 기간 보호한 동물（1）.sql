77adbcf775e45c759cd002432d7ab54117a643e9
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-12 20:14:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT name, datetime
+select name, datetime
 from animal_ins
-where animal_id not in (select animal_id from animal_outs)
+where ANIMAL_ID not in (select ANIMAL_ID from animal_outs)
 order by datetime
-limit 3;
+limit 3
