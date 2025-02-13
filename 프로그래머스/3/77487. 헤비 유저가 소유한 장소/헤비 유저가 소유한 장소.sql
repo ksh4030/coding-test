b5bb06503843a690df69aa94ed9e1b59225690e1
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-13 23:45:24 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,9 @@
--- 코드를 입력하세요
-SELECT * from places
+select *
+from places
 where host_id in (
-    select host_id 
+    select host_id
     from places
     group by host_id
-    having count(host_id) >= 2
+    having count(host_id) > 1
 )
 order by id;
