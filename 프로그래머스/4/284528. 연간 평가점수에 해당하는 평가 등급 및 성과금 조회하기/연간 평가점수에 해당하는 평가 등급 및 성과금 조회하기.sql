a930d46674d5f637033bf2f21fc0b10745f6b948
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-18 15:03:10 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,19 +1,17 @@
--- 코드를 작성해주세요
-select a.emp_no, a.emp_name,
-(case 
-    when avg(score) >= 96 then 'S'
-    when avg(score) >= 90 then 'A'
-    when avg(score) >= 80 then 'B'
-    else 'C' end
-) as grade,
-(case
-    when avg(score) >= 96 then a.sal * 0.2
-    when avg(score) >= 90 then a.sal * 0.15
-    when avg(score) >= 80 then a.sal * 0.1
-    else 0 end
-) as bonus
-from hr_employees a
-join hr_grade b on a.emp_no = b.emp_no
-group by a.emp_no
-order by a.emp_no
-;
+select hg.emp_no, he.emp_name, 
+case
+    when avg(hg.score) >= 96 then 'S'
+    when avg(hg.score) >= 90 then 'A'
+    when avg(hg.score) >= 80 then 'B'
+    else 'C'
+end as grade,
+case
+    when avg(hg.score) >= 96 then he.sal * 0.2
+    when avg(hg.score) >= 90 then he.sal * 0.15
+    when avg(hg.score) >= 80 then he.sal * 0.1
+    else 0
+end as bonus
+from hr_grade hg
+join hr_employees he on hg.emp_no = he.emp_no
+group by hg.emp_no
+order by hg.emp_no
