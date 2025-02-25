d06076675de074ff785ca6eb8871eb9a3a91ce53
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-25 17:21:23 +0900

    [level 3] Title: 야근 지수, Time: 164.27 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -2,22 +2,23 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (int a : works) {
-            pq.add(a);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            return o2 - o1;
+        });
+        
+        for(int num : works) pq.add(num);
+        
+        for(int i=0; i<n; i++) {
+            if(pq.isEmpty()) break;
+            int bucket = pq.poll();
+            if(bucket>0) pq.add(bucket - 1);
         }
-
-        for (int i = 0; i < n; i++) {
-            int a = pq.poll();
-            if(a<=0) break;
-            pq.add(a-1);
+        
+        while(!pq.isEmpty()) {
+            int bucket = pq.poll();
+            answer += (bucket * bucket);
         }
-
-        int size = pq.size();
-        for (int i = 0; i < size; i++) {
-            int a = pq.poll();
-            answer += (a*a);
-        }
+                
         return answer;
     }
 }
