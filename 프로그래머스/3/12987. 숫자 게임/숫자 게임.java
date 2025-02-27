6189b021b4ec935441c586fe559c10e707880c95
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-02-27 17:10:27 +0900

    [level 3] Title: 숫자 게임, Time: 106.37 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -2,20 +2,19 @@ import java.util.*;
 class Solution {
     public int solution(int[] A, int[] B) {
         int answer = 0;
+        
         Arrays.sort(A);
         Arrays.sort(B);
+        int idxA = 0;
+        int idxB = 0;
         
-        int a=0, b=0;
-        for(int i=0; i<A.length; i++) {
-            try{
-                if(!(A[a]<B[b])) b++;
-                else {
-                    a++;
-                    b++;
-                    answer++;
-                }
-            } catch (Exception e) {
-                break;
+        while(idxB < B.length) {
+            if(A[idxA] < B[idxB]) {
+                answer++;
+                idxA++;
+                idxB++;
+            } else {
+                idxB++;
             }
         }
         
