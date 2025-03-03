ea580b782ffdeeb07f81d2ad9494ed1decd78d1c
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-03-03 14:43:55 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,4 +1,8 @@
-select animal_id, name
+-- 코드를 입력하세요
+SELECT animal_id, name
 from animal_outs
-where animal_id not in (select animal_id from animal_ins)
+where animal_id not in (
+    select animal_id
+    from animal_ins
+)
 order by animal_id;
