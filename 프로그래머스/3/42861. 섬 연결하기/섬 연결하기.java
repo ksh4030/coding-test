88f473f2b462c0870d17ec18fc301f189e7e1419
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-03-18 14:11:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.70 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,15 +2,21 @@ import java.util.*;
 class Solution {
     static int[] parent;
     public int solution(int n, int[][] costs) {
-        int answer = 0;
         init(n, costs);
-        answer = kruskal(n, costs);
-        return answer;
+        return kruskal(n, costs);
+    }
+    
+    public void init(int n, int[][] costs) {
+        parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
+        
+        Arrays.sort(costs, (o1, o2) -> o1[2]-o2[2]);
     }
     
     public int kruskal(int n, int[][] costs) {
-        int cost = 0;
         int cnt = 0;
+        int cost = 0;
+        
         for(int[] arr : costs) {
             if(union(arr[0], arr[1])) {
                 cost += arr[2];
@@ -18,12 +24,14 @@ class Solution {
                 if(cnt == n-1) break;
             }
         }
+        
         return cost;
     }
     
     public boolean union(int num1, int num2) {
         int r1 = find(num1);
         int r2 = find(num2);
+        
         if(r1 == r2) return false;
         parent[r2] = r1;
         return true;
@@ -32,12 +40,5 @@ class Solution {
     public int find(int num) {
         if(parent[num] != num) parent[num] = find(parent[num]);
         return parent[num];
-    }
-    
-    public void init(int n, int[][] costs) {
-        parent = new int[n];
-        for(int i=0; i<n; i++) parent[i] = i;
-        
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
     }
 }
