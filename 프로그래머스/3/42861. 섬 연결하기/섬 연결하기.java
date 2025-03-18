cb2e1b7ae55255ae2f6de635c32363fc48da83dd
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-03-18 14:03:21 +0900

    [level 3] Title: 섬 연결하기, Time: 0.69 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,41 +1,43 @@
 import java.util.*;
-
 class Solution {
-    int[] parent; // 유니온-파인드 배열
-
-    // find 연산 (경로 압축)
-    private int find(int v) {
-        if (parent[v] != v) parent[v] = find(parent[v]); // 부모 갱신
-        return parent[v];
-    }
-
-    // union 연산 (사이클 방지)
-    private boolean union(int v1, int v2) {
-        int root1 = find(v1);
-        int root2 = find(v2);
-        if (root1 == root2) return false; // 같은 집합이면 합치지 않음
-        parent[root2] = root1; // 하나의 집합으로 연결
-        return true;
-    }
-
+    static int[] parent;
     public int solution(int n, int[][] costs) {
-        // 1. 간선 정렬 (비용 기준)
-        Arrays.sort(costs, Comparator.comparingInt(e -> e[2]));
-
-        // 2. 유니온-파인드 배열 초기화
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        // 3. 크루스칼 알고리즘 실행
-        int mstCost = 0, edgeCount = 0;
-        for (int[] cost : costs) {
-            if (union(cost[0], cost[1])) { // 사이클 방지
-                mstCost += cost[2];
-                edgeCount++;
-                if (edgeCount == n - 1) break; // MST 완성
+        int answer = 0;
+        init(n, costs);
+        answer = kruskal(n, costs);
+        return answer;
+    }
+    
+    public int kruskal(int n, int[][] costs) {
+        int cost = 0;
+        int cnt = 0;
+        for(int[] arr : costs) {
+            if(union(arr[0], arr[1])) {
+                cost += arr[2];
+                cnt++;
+                if(cnt == n-1) break;
             }
         }
-
-        return mstCost;
+        return cost;
+    }
+    
+    public boolean union(int num1, int num2) {
+        int r1 = find(num1);
+        int r2 = find(num2);
+        if(r1 == r2) return false;
+        parent[r2] = r1;
+        return true;
+    }
+    
+    public int find(int num) {
+        if(parent[num] != num) parent[num] = find(parent[num]);
+        return parent[num];
+    }
+    
+    public void init(int n, int[][] costs) {
+        parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
+        
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
     }
 }
