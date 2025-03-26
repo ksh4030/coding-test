beaff0ed7cf9006e080244987c8f71d1f1ac222e
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-03-27 01:59:39 +0900

    [level 3] Title: 등굣길, Time: 0.70 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#qna) 
 
 ### 성능 요약
 
-메모리: 53.7 MB, 시간: 0.76 ms
+메모리: 54.3 MB, 시간: 0.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 16:09:10
+2025년 03월 27일 01:59:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,21 @@
 import java.util.*;
 class Solution {
-    static final int MOD = 1_000_000_007;
     static int[][] map;
     static boolean[][] isPuddles;
-    
+    static final int MOD = 1_000_000_007;
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        
         init(m, n, puddles);
         dp();
-        
         return map[m][n];
     }
     
     public void dp() {
         for(int i=1; i<map.length; i++) {
-            for(int j=1; j<map[i].length; j++) {
+            for(int j=1; j<map[0].length; j++) {
                 if(i==1 && j==1) continue;
                 if(!isPuddles[i][j]) {
+                    // map[i][j] = (map[i-1][j] % MOD) + (map[i][j-1] % MOD);
                     map[i][j] = (map[i-1][j] + map[i][j-1]) % MOD;
                 }
             }
@@ -26,8 +24,9 @@ class Solution {
     
     public void init(int m, int n, int[][] puddles) {
         map = new int[m+1][n+1];
-        isPuddles = new boolean[m+1][n+1];
         map[1][1] = 1;
+        
+        isPuddles = new boolean[m+1][n+1];
         
         for(int[] arr : puddles) {
             isPuddles[arr[0]][arr[1]] = true;
