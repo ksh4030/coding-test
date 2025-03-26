6cfd4bcdbb5206475a653f61b5f92135ddb6f63d
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-03-26 20:51:11 +0900

    [level 3] Title: N으로 표현, Time: 125.74 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,36 +1,28 @@
 import java.util.*;
-
 class Solution {
+    static List<Set<Integer>> list = new ArrayList<>();
     public int solution(int N, int number) {
-        int answer = 0;
         if(N == number) return 1;
-        return find(N, number);
-    }
-    
-    public int find(int N, int number) {
-        List<Set<Integer>> list = new ArrayList<>();
-        for(int i=0; i<=8; i++) {
-            list.add(new HashSet<>());
-        }
+        
+        for(int i=0; i<=8; i++) list.add(new HashSet<>());
         list.get(1).add(N);
-
+        
         for(int i=2; i<=8; i++) {
-            int curNum = Integer.parseInt(String.valueOf(N).repeat(i));
-            list.get(i).add(curNum);
             for(int j=1; j<i; j++) {
                 for(int num1 : list.get(j)) {
                     for(int num2 : list.get(i-j)) {
                         list.get(i).add(num1 + num2);
                         list.get(i).add(num1 - num2);
                         list.get(i).add(num1 * num2);
-                        if(num2 != 0) {
-                            list.get(i).add(num1 / num2);
-                        }
+                        if(num2 > 0) list.get(i).add(num1 / num2);
+                        list.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i)));
                     }
                 }
             }
-            if(list.get(i).contains(number)) return i;
+            for(int num : list.get(i)) if(num == number) return i;
         }
+        
         return -1;
     }
 }
+
