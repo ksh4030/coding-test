50ecef36a7b19e0e64a7d64706954932e9a3e170
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-05-01 22:45:55 +0900

    [level 3] Title: 가장 먼 노드, Time: 1253.52 ms, Memory: 627 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,16 +1,15 @@
 import java.util.*;
 class Solution {
-    List<List<Integer>> graph = new ArrayList<>();
+    static boolean[][] board;
     static boolean[] v;
     public int solution(int n, int[][] edge) {
         int answer = 0;
+        board = new boolean[n+1][n+1];
         v = new boolean[n+1];
         
-        for(int i=0; i<=n; i++) graph.add(new ArrayList<>());
-        
-        for(int[] arr : edge) {
-            graph.get(arr[0]).add(arr[1]);
-            graph.get(arr[1]).add(arr[0]);
+        for(int[] e : edge) {
+            board[e[0]][e[1]] = true;
+            board[e[1]][e[0]] = true;
         }
         
         return bfs();
@@ -20,29 +19,30 @@ class Solution {
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(1, 0));
         v[1] = true;
+        
         int max = 0;
-        int ans = 0;
+        int cnt = 0;
         
         while(!q.isEmpty()) {
             Node cur = q.poll();
-            if(cur.cnt == max) ans++;
-            if(cur.cnt > max) {
-                ans = 1;
-                max = cur.cnt;
-            }
             
-            for(int i=0; i<graph.get(cur.num).size(); i++) {
-                int next = graph.get(cur.num).get(i);
-                if(!v[next]) {
-                    v[next] = true;
-                    q.add(new Node(next, cur.cnt+1));
+            for(int i=1; i<board[cur.num].length; i++) {
+                if(board[cur.num][i] && !v[i]) {
+                    q.add(new Node(i, cur.cnt+1));
+                    v[i] = true;
+                    if(max == cur.cnt+1) {
+                        cnt++;
+                    } else if (max < cur.cnt+1) {
+                        max = cur.cnt+1;
+                        cnt = 1;
+                    }
                 }
             }
         }
         
-        return ans;
+        return cnt;
     }
-    
+
     class Node {
         int num, cnt;
         public Node(int num, int cnt) {
