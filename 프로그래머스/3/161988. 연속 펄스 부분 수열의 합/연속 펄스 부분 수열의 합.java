d3c1e35f2bff43466d02ca91f5af97af2c7ea5b6
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-05-03 20:48:25 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 26.20 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.java
@@ -1,10 +1,27 @@
 import java.util.*;
 class Solution {
+    static int[] a, b;
     public long solution(int[] sequence) {
         long answer = 0;
+        init(sequence);
+        answer = sw(a);        
+        return Math.max(answer, sw(b));
+    }
+    
+    public long sw(int[] arr) {
+        long sum = Integer.MIN_VALUE;
+        long max = 0;
         
-        int[] a = new int[sequence.length];
-        int[] b = new int[sequence.length];
+        for(int i=0; i<arr.length; i++) {
+            sum = Math.max(sum+arr[i], arr[i]);
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+    
+    public void init(int[] sequence) {
+        a = new int[sequence.length];
+        b = new int[sequence.length];
         
         for(int i=0; i<sequence.length; i++) {
             if(i%2 == 0) {
@@ -14,20 +31,6 @@ class Solution {
                 a[i] = sequence[i] * -1;
                 b[i] = sequence[i];
             }
-        }        
-        answer = Math.max(kadane(a), kadane(b));        
-        return answer;
-    }
-    
-    public static long kadane(int[] arr) {
-        long max = 0;
-        long cur = 0;
-        
-        for(int i=0; i<arr.length; i++) {
-            cur = Math.max(arr[i], cur+arr[i]);
-            max = Math.max(cur, max);
         }
-        
-        return max;
     }
 }
