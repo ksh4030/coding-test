57a39aa54d2b589928e03c49b4584733001de5d4
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-05-08 23:12:08 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 7.29 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.java
@@ -1,56 +1,47 @@
 import java.util.*;
-
 class Solution {
-    final static int NINE = 540; // 09:00 in minutes
-    
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static int start = 540;
     public String solution(int n, int t, int m, String[] timetable) {
-        // 1. timetable을 분으로 변환하여 정렬
-        int[] time = new int[timetable.length];
-        for (int i = 0; i < time.length; i++) {
-            time[i] = tToI(timetable[i]);
-        }
-        Arrays.sort(time);
+        String answer = "";
+        StringToInt(timetable);
         
-        int idx = 0; // timetable의 인덱스
-        int cur = NINE; // 현재 셔틀의 도착 시각 (분 단위)
-        
-        // 2. 셔틀이 n-1번 도착할 때까지 크루를 태움
-        for (int i = 0; i < n - 1; i++) {
-            int capacity = m;
-            while (idx < time.length && time[idx] <= cur && capacity > 0) {
-                idx++;
-                capacity--;
+        for(int i=0; i<n-1; i++) {
+            for(int j=0; j<m; j++) {
+                if(!pq.isEmpty() && pq.peek() <= start) pq.poll();
             }
-            cur += t;
+            start += t;
         }
         
-        // 3. 마지막 셔틀의 경우
-        int capacity = m;
-        while (idx < time.length && time[idx] <= cur && capacity > 0) {
-            idx++;
-            capacity--;
+        int cnt = 0;
+        int last = 0;
+        for(int i=0; i<m; i++) {
+            if(!pq.isEmpty() && pq.peek() <= start) {
+                last = pq.poll();
+                cnt++;
+            }
         }
         
-        // 4. 콘이 탈 수 있는 가장 늦은 시간을 계산
-        if (capacity > 0) {
-            // 셔틀에 자리가 남아있는 경우
-            return iToT(cur);
+        if(cnt < m) {
+            answer = intToString(start);
         } else {
-            // 셔틀에 자리가 없는 경우, 마지막으로 탄 크루보다 1분 빨리 도착해야 함
-            return iToT(time[idx - 1] - 1);
+            answer = intToString(last - 1);
         }
+        
+        return answer;
     }
     
-    // HH:MM 형식을 분으로 변환
-    public int tToI(String s) {
-        String[] bucket = s.split(":");
-        return Integer.parseInt(bucket[0]) * 60 + Integer.parseInt(bucket[1]);
+    public String intToString(int time) {
+        int h = time / 60;
+        int m = time % 60;
+        
+        return String.format("%02d:%02d", h, m);
     }
     
-    // 분을 HH:MM 형식으로 변환
-    public String iToT(int n) {
-        int hours = n / 60;
-        int minutes = n % 60;
-        return String.format("%02d:%02d", hours, minutes);
+    public void StringToInt(String[] timetable) {
+        for(String s : timetable) {
+            String[] arr = s.split(":");
+            pq.add(Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]));
+        }
     }
 }
