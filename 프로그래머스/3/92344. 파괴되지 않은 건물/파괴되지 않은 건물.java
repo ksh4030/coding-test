44155fceb04b765bdc6b18f3d36140c6afd0098a
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-05-11 17:17:03 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 53.15 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,8 +1,22 @@
 import java.util.*;
 class Solution {
+    static int[][] arr;
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
-        int[][] arr = new int[board.length+1][board[0].length+1];
+        init(board.length, board[0].length, skill);
+        
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[0].length; j++) {
+                board[i][j] += arr[i][j];
+                if(board[i][j] > 0) answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void init(int r, int c, int[][] skill) {
+        arr = new int[r+1][c+1];
         
         for(int[] a : skill) {
             int type = a[0];
@@ -10,13 +24,14 @@ class Solution {
             int c1 = a[2];
             int r2 = a[3];
             int c2 = a[4];
-            int damage = a[5];
+            int degree = a[5];
             
-            if(type == 1) damage = -damage;
-            arr[r1][c1] += damage;
-            arr[r2+1][c1] += -damage;
-            arr[r1][c2+1] += -damage;
-            arr[r2+1][c2+1] += damage;
+            degree = type == 1 ? -degree : degree;
+            
+            arr[r1][c1] += degree;
+            arr[r1][c2+1] -= degree;
+            arr[r2+1][c1] -= degree;
+            arr[r2+1][c2+1] += degree;
         }
         
         for(int i=0; i<arr.length; i++) {
@@ -30,14 +45,5 @@ class Solution {
                 arr[j+1][i] += arr[j][i];
             }
         }
-        
-        for(int i=0; i<board.length; i++) {
-            for(int j=0; j<board[0].length; j++) {
-                board[i][j] += arr[i][j];
-                if(board[i][j] > 0) answer++;
-            }
-        }
-        
-        return answer;
     }
 }
