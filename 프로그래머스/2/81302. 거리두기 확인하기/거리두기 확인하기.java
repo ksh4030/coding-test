6505d2c7d08ba442731f2de0932e38e9dfb7d2c1
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-04 17:17:50 +0900

    [level 2] Title: 거리두기 확인하기, Time: 5.36 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,58 +1,63 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {-1,1,0,0};
-    static int[] dc = {0,0,-1,1};
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
     public int[] solution(String[][] places) {
         int[] answer = new int[places.length];
         
         for(int i=0; i<places.length; i++) {
-            answer[i] = checkMap(places[i]) ? 1 : 0;
+            String[][] map = makeMap(places[i]);
+            answer[i] = checkMap(map) ? 1 : 0;
         }
         
         return answer;
     }
     
-    public boolean checkMap(String[] place) {
-        String[][] map = new String[place.length][place[0].length()];
-        for(int i=0; i<place.length; i++) map[i] = place[i].split("");
-        
+    public boolean checkMap(String[][] map) {
         for(int i=0; i<map.length; i++) {
             for(int j=0; j<map[0].length; j++) {
                 if(map[i][j].equals("P")) {
-                    if(!bfs(new Pos(i,j,0), map)) return false;
+                    boolean[][] v = new boolean[map.length][map[0].length];
+                    if(!isPossible(i, j, v, map)) return false;
                 }
             }
         }
+        
         return true;
     }
     
-    public boolean bfs(Pos pos, String[][] map) {
-        boolean[][] v = new boolean[map.length][map[0].length];
-        v[pos.r][pos.c] = true;
-        Queue<Pos> q = new LinkedList<>();
-        q.add(pos);
+    public boolean isPossible(int r, int c, boolean[][] v, String[][] map) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(r, c, 0));
+        v[r][c] = true;
         
         while(!q.isEmpty()) {
-            Pos cur = q.poll();
+            Node cur = q.poll();
             if(cur.cnt > 2) continue;
-            if(cur.cnt > 0 && cur.cnt <= 2 && map[cur.r][cur.c].equals("P")) return false;
+            if(cur.cnt != 0 && map[cur.r][cur.c].equals("P")) return false;
             
             for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
-            
+                
                 if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || v[nr][nc] || map[nr][nc].equals("X")) continue;
                 v[nr][nc] = true;
-                q.add(new Pos(nr, nc, cur.cnt+1));
+                q.add(new Node(nr, nc, cur.cnt+1));
             }
         }
         
         return true;
     }
     
-    class Pos {
+    public String[][] makeMap(String[] place) {
+        String[][] map = new String[place.length][place[0].length()];
+        for(int i=0; i<place.length; i++) map[i] = place[i].split("");
+        return map;
+    }
+    
+    class Node{
         int r, c, cnt;
-        public Pos(int r, int c, int cnt) {
+        public Node(int r, int c, int cnt) {
             this.r = r;
             this.c = c;
             this.cnt = cnt;
