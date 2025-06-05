a4b8c3004d765f3e6bcebbc422a44ac291121bad
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-05 22:40:51 +0900

    [level 2] Title: 광물 캐기, Time: 16.62 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,62 +1,51 @@
 import java.util.*;
 class Solution {
-        static final int[][] score = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+    static int[][] tired = {{1,1,1}, {5,1,1}, {25,5,1}};
+    static int answer = Integer.MAX_VALUE;
     public int solution(int[] picks, String[] minerals) {
-        int answer = 0;
-        int cnt = Arrays.stream(picks).sum();
-        List<Node> list = new ArrayList<>();
-        for (int i = 0; i < minerals.length; i+=5) {
-            if(cnt == 0) break;
-
-            int da=0, ir=0, st=0;
-            for (int j = i; j < i+5; j++) {
-                int n = 0;
-                try {
-                    n = minerals[j].equals("iron") ? 1 : minerals[j].equals("stone") ? 2: 0;
-                } catch (Exception e) {
-                    break;
-                }
-                da += score[0][n];
-                ir += score[1][n];
-                st += score[2][n];
-            }
-            list.add(new Node(da, ir, st));
-            cnt--;
-        }
-
-        list.sort((o1, o2) -> o2.stone - o1.stone);
-        for(Node m : list) {
-            int dia = m.diamond;
-            int iron = m.iron;
-            int stone = m.stone;
-
-            if(picks[0] > 0) {
-                answer += dia;
-                picks[0]--;
-                continue;
-            }
-            if(picks[1] > 0) {
-                answer += iron;
-                picks[1]--;
-                continue;
-            }
-            if(picks[2] > 0) {
-                answer += stone;
-                picks[2]--;
-                continue;
-            }
-        }
+        dfs(0,0, picks, minerals);
         return answer;
     }
     
-    
-    public static class Node {
-        int diamond, iron, stone;
-
-        public Node(int diamond, int iron, int stone) {
-            this.diamond = diamond;
-            this.iron = iron;
-            this.stone = stone;
+    public void dfs(int idx, int sum, int[] picks, String[] minerals) {
+        if(idx >= minerals.length) {
+            answer = Math.min(sum, answer);
+            return;
+        }
+        
+        if(Arrays.stream(picks).sum() == 0) {
+            answer = Math.min(sum, answer);
+            return;
+        }
+        
+        for(int i=0; i<3; i++) {
+            if(picks[i] > 0) {
+                picks[i]--;
+                int n = cal(idx, idx+5>=minerals.length ? minerals.length : idx+5, minerals, i);
+                dfs(idx+5, sum+n, picks, minerals);
+                picks[i]++;
+            }
         }
     }
+    
+    public int cal(int start, int end, String[] minerals, int cur) {
+        int sum = 0;
+        
+        for(int i=start; i<end; i++) {
+            switch(minerals[i]) {
+                case "diamond" :
+                    sum += tired[cur][0];
+                    break;
+                case "iron" :
+                    sum += tired[cur][1];
+                    break;
+                case "stone" :
+                    sum += tired[cur][2];
+                    break;
+            }
+        }
+        
+        return sum;
+    }
+    
 }
