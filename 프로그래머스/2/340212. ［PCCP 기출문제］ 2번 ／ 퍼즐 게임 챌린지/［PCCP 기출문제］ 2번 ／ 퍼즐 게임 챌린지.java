eb06393230fd1246daf14d9991ed320b57c6df7a
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-07 16:46:12 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 50.89 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,36 +1,37 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int first = 1;
-        int last = 100_000; 
-        int answer = last; 
+        int left = 1;
+        int right = Arrays.stream(diffs).max().getAsInt();
+        int answer = right;
         
-        while (first <= last) {
-            int mid = (first + last) / 2;
-            long time_total = 0;
-            long time_prev = 0; 
-
-            for (int i = 0; i < diffs.length; i++) {
-                if (diffs[i] <= mid) {
-                    time_total += times[i];
-                } else {
-                    time_total += (diffs[i] - mid) * (time_prev + times[i]) + times[i]; 
-                }
-                time_prev = times[i]; 
-                if (time_total > limit) {
-                    break; 
-                }
-            }
-
-            if (time_total > limit) {
-                first = mid + 1; 
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            
+            if(isPossible(mid, diffs, times, limit)) {
+                answer = mid;
+                right = mid - 1;
             } else {
-                answer = Math.min(answer, mid);
-                last = mid - 1;
+                left = mid + 1;
             }
         }
         
         return answer;
+    }
+    
+    public boolean isPossible(int mid, int[] diffs, int[] times, long limit) {
+        long sum = 0;
+        for(int i=0; i<diffs.length; i++) {
+            if(mid < diffs[i]) {
+                int cnt = diffs[i] - mid;
+                int time = i == 0 ? 0 : times[i] + times[i-1];
+                sum += (long)(cnt * time) + times[i];
+            } else {
+                sum += times[i];
+            }
+            
+            if(sum > limit) return false;
+        }
+        return true;
     }
 }
