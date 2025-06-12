edd9b7da0314da0d0fcd0203e923e15446c095ba
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-12 17:21:39 +0900

    [level 2] Title: 지게차와 크레인, Time: 78.41 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,101 +1,98 @@
 import java.util.*;
 class Solution {
     static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+    static int[] dc = {0, -1, 0, 1};
+    
     static String[][] map;
     public int solution(String[] storage, String[] requests) {
         int answer = 0;
-        
-        makeMap(storage);
+
+        init(storage);
         for(String s : requests) {
             if(s.length() > 1) {
                 crane(String.valueOf(s.charAt(0)));
             } else {
-                car(s);
+                lift(s);
             }
         }
         
-        for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[i].length; j++) {
-                if(!map[i][j].equals("NONE")) answer++;
-            }
-        }
-        
-        return answer;
+        return cntAns();
     }
     
-    public void crane(String target) {
-        for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[i].length; j++) {
-                if(map[i][j].equals(target)) {
-                    map[i][j] = "NONE";
-                }
+    public int cntAns() {
+        int sum = 0;
+        for(String[] arr : map) {
+            for(String s : arr) {
+                if(!s.equals("")) sum++;
             }
         }
+        return sum;
     }
     
-    public void car(String target) {
-        List<Pos> list = new ArrayList<>();
+    public void lift(String target) {
+        List<Node> list = new ArrayList<>();
         for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[i].length; j++) {
-                if(map[i][j].equals(target)) {
-                    list.add(new Pos(i,j));
+            for(int j=0; j<map[0].length; j++) {
+                if(map[i][j].equals(target) && isPossible(i, j)) {
+                    list.add(new Node(i,j));
                 }
             }
         }
         
-        boolean[] checked = new boolean[list.size()];
         for(int i=0; i<list.size(); i++) {
-            if(isPossible(list.get(i), target)) {
-                checked[i] = true;
-            }
-        }
-        
-        for(int i=0; i<checked.length; i++) {
-            if(checked[i]) {
-                int r = list.get(i).r;
-                int c = list.get(i).c;
-                map[r][c] = "NONE";
-            }
+            int r = list.get(i).r;
+            int c = list.get(i).c;
+            
+            map[r][c] = "";
         }
     }
     
-    public boolean isPossible(Pos pos, String target) {
-        boolean[][] v = new boolean[map.length][map[0].length];
+    public boolean isPossible(int r, int c) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(r,c));
         
-        Queue<Pos> q = new LinkedList<>();
-        q.add(new Pos(pos.r, pos.c));
-        v[pos.r][pos.c] = true;
+        boolean[][] v = new boolean[map.length][map[0].length];
+        v[r][c] = true;
         
         while(!q.isEmpty()) {
-            Pos cur = q.poll();
-            if(cur.r == 0 || cur.c == 0 || cur.r==map.length-1 || cur.c==map[0].length-1) return true;
+            Node cur = q.poll();
             
             for(int i=0; i<4; i++) {
                 int nr = dr[i] + cur.r;
                 int nc = dc[i] + cur.c;
                 
-                if(nr>=0 && nc>=0 && nr<map.length && nc<map[0].length && !v[nr][nc] && map[nr][nc].equals("NONE")) {
-                    v[nr][nc] = true;
-                    q.add(new Pos(nr, nc));
-                }
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length) return true;
+                if(!map[nr][nc].equals("") || v[nr][nc]) continue;
+                v[nr][nc] = true;
+                q.add(new Node(nr, nc));
             }
         }
-        
         return false;
     }
     
-    public void makeMap(String[] storage) {
-        map = new String[storage.length][storage[0].length()];
-        
+    public void crane(String target) {
         for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[0].length; j++) {
+                if(map[i][j].equals(target)) {
+                    map[i][j] = "";
+                }
+            }
+        }
+    }
+    
+    public void init(String[] storage) {
+        int r = storage.length;
+        int c = storage[0].length();
+        
+        map = new String[r][c];
+        for(int i=0; i<r; i++) {
             map[i] = storage[i].split("");
         }
     }
     
-    class Pos {
+    class Node {
         int r, c;
-        public Pos(int r, int c) {
+        public Node(int r, int c) {
             this.r = r;
             this.c = c;
         }
