983f6e8385e4b39ff526a64353fd6e69002562bf
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-12 14:08:36 +0900

    [level 2] Title: 비밀 코드 해독, Time: 38.92 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,41 +1,32 @@
 import java.util.*;
 class Solution {
     static int answer = 0;
-    static int[] arr;
     public int solution(int n, int[][] q, int[] ans) {        
-        arr = new int[n];
-        for(int i=0; i<n; i++) arr[i] = i+1;
-        comb(n, q, ans, 0, new ArrayList<>());
+        comb(1, 5, n, new ArrayList<>(), q, ans);
+        
         return answer;
     }
     
-    public void comb(int n, int[][] q, int[] ans, int cur, List<Integer> list) {
-        if(list.size() == 5) {
-            // System.out.println(list);
-            if(isPossible(q, ans, list)) answer++;
+    public void comb(int cur, int r, int n, List<Integer> list, int[][] q, int[] ans) {
+        if(list.size() == r) {
+            if(isPossible(list, q, ans)) answer++;
             return;
         }
         
-        for(int i=cur; i<n; i++) {
-            list.add(arr[i]);
-            comb(n, q, ans, i+1, list);
-            list.remove(list.size() - 1);
+        for(int i=cur; i<=n; i++) {
+            list.add(i);
+            comb(i+1, r, n, list, q, ans);
+            list.remove(list.size()-1);
         }
     }
     
-    public boolean isPossible(int[][] q, int[] ans, List<Integer> list) {
+    public boolean isPossible(List<Integer> list, int[][] q, int[] ans) {
         for(int i=0; i<q.length; i++) {
             int cnt = 0;
             for(int j=0; j<q[i].length; j++) {
-                for(int k=0; k<list.size(); k++) {
-                    if(q[i][j] == list.get(k)){
-                        cnt++;
-                        break;
-                    }
-                }
+                if(list.contains(q[i][j])) cnt++;
             }
-            
-            if(cnt != ans[i]) return false;
+            if(ans[i] != cnt) return false;
         }
         return true;
     }
