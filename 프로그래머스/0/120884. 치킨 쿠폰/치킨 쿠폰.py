b1a1fddac903249893766905bcad11c3779b7adc
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-20 15:12:25 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,11 @@
+def solution(chicken):
+    answer = 0
+    coupon = chicken
+    
+    while coupon >= 10 :
+        answer += coupon // 10
+        coupon = (coupon // 10) + (coupon % 10)
+        
+        print(coupon)
+    
+    return answer
