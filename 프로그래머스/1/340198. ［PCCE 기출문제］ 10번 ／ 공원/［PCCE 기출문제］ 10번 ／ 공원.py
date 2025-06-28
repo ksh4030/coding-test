639cb590dd8ca7f8ca7a81b003b2180f138e6b17
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-28 23:20:36 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 61.77 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.py
@@ -1,19 +1,21 @@
 def solution(mats, park):
-    rows = len(park)
-    cols = len(park[0])
+    answer = -1
+    
+    for i in range(len(park)) :
+        for j in range(len(park[i])) :
+            if park[i][j] == "-1" :
+                for n in mats :
+                    if isPossible(i,j,n,park) :
+                        answer = max(answer, n)
+    
+    return answer
 
-    # 큰 돗자리부터 시도
-    for mat in sorted(mats, reverse=True):
-        for i in range(rows - mat + 1):  # 돗자리가 안 넘치게
-            for j in range(cols - mat + 1):
-                flag = True
-                for l in range(mat):
-                    for m in range(mat):
-                        if park[i + l][j + m] != "-1":
-                            flag = False
-                            break
-                    if not flag:
-                        break
-                if flag:
-                    return mat  # 가장 큰 돗자리를 찾았으므로 바로 리턴
-    return -1
+def isPossible(r, c, size, park) :
+    for i in range(size) :
+        for j in range(size) :
+            nr = r+i
+            nc = c+j
+            
+            if nr>=len(park) or nc>=len(park[0]) or park[nr][nc] != "-1" :
+                return False
+    return True
