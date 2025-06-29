6f0842b09f004fa613c3d6dc070f229baf00256b
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-29 17:05:29 +0900

    [level 3] Title: 단속카메라, Time: 26.51 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -7,14 +7,16 @@ class Solution {
             return o1[1] - o2[1];
         });
         
+        boolean[] used = new boolean[routes.length];
+        used[0] = true;
+        int[] cur = routes[0];
+        
         System.out.println(Arrays.deepToString(routes));
         
-        int cur = routes[0][1];
-        
         for(int i=1; i<routes.length; i++) {
-            if(cur < routes[i][0]) {
+            if(cur[1] < routes[i][0]) {
                 answer++;
-                cur = routes[i][1];
+                cur = routes[i];
             }
         }
         
