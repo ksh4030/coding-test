22d2ab22e890faea49a0bf80e79535aea3dc607a
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-06-30 23:27:13 +0900

    [level 3] Title: 스티커 모으기(2), Time: 10.71 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -3,17 +3,18 @@ class Solution {
     public int solution(int sticker[]) {
         int answer = 0;
         if(sticker.length == 1) return sticker[0];
-        int[] a = new int[sticker.length];
-        int[] b = new int[sticker.length];
+
+        int[] arr1 = new int[sticker.length];
+        int[] arr2 = new int[sticker.length];
         
-        a[0] = a[1] = sticker[0];
-        b[1] = sticker[1];
+        arr1[0] = arr1[1] = sticker[0];
+        arr2[1] = sticker[1];
         
         for(int i=2; i<sticker.length; i++) {
-            if(i != sticker.length-1)a[i] = Math.max(a[i-2]+sticker[i], a[i-1]);            
-            b[i] = Math.max(b[i-2]+sticker[i], b[i-1]);
+            if(i < sticker.length-1) arr1[i] = Math.max(arr1[i-2]+sticker[i], arr1[i-1]);
+            arr2[i] = Math.max(arr2[i-2]+sticker[i], arr2[i-1]);
         }
 
-        return Math.max(a[a.length-2], b[b.length-1]);
+        return Math.max(arr1[arr1.length-2], arr2[arr2.length-1]);
     }
 }
