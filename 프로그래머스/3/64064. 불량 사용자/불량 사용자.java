2c363efba1ded1d3fb3d0068a12e08af89cfa539
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-01 00:01:03 +0900

    [level 3] Title: 불량 사용자, Time: 55.64 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,39 +1,43 @@
 import java.util.*;
+
 class Solution {
-    static boolean[] v;
-    static Set<String> set;
+    Set<Set<String>> answerSet = new HashSet<>();
+
     public int solution(String[] user_id, String[] banned_id) {
-        int answer = 0;
-        v = new boolean[user_id.length];
-        set = new HashSet<>();
-
-        for (int i = 0; i < banned_id.length; i++) {
-            banned_id[i] = banned_id[i].replace("*", ".");
-        }
-
-        comb(0, "", user_id, banned_id);
-        return set.size();
+        boolean[] visited = new boolean[user_id.length];
+        dfs(0, user_id, banned_id, visited, new HashSet<>());
+        return answerSet.size();
     }
-    
-        public static void comb(int depth, String s, String[] user_id, String[] banned_id) {
-        if(depth == banned_id.length) {
-            String[] arr = s.split(" ");
-            Arrays.sort(arr);
 
-            String bucket = "";
-            for (String a : arr) {
-                bucket += a;
-            }
-            set.add(bucket);
+    void dfs(int depth, String[] user_id, String[] banned_id, boolean[] visited, Set<String> currentSet) {
+        if (depth == banned_id.length) {
+            // 조합을 복사하여 저장 (HashSet은 순서 상관 없이 동일한 요소면 중복 제거됨)
+            answerSet.add(new HashSet<>(currentSet));
             return;
         }
 
         for (int i = 0; i < user_id.length; i++) {
-            if(!v[i] && user_id[i].matches(banned_id[depth])) {
-                v[i] = true;
-                comb(depth+1, user_id[i]+" "+s, user_id, banned_id);
-                v[i] = false;
-            }
+            if (visited[i]) continue;
+            if (!isMatch(user_id[i], banned_id[depth])) continue;
+
+            visited[i] = true;
+            currentSet.add(user_id[i]);
+
+            dfs(depth + 1, user_id, banned_id, visited, currentSet);
+
+            visited[i] = false;
+            currentSet.remove(user_id[i]);
         }
+    }
+
+    boolean isMatch(String user, String ban) {
+        if (user.length() != ban.length()) return false;
+
+        for (int i = 0; i < user.length(); i++) {
+            if (ban.charAt(i) == '*') continue;
+            if (user.charAt(i) != ban.charAt(i)) return false;
+        }
+
+        return true;
     }
 }
