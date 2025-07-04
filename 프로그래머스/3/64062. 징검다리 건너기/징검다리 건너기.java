87525c4b1c6f419a8bf1c738b85e60edc489f4bb
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-04 14:30:32 +0900

    [level 3] Title: 징검다리 건너기, Time: 65.68 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -3,30 +3,34 @@ class Solution {
     public int solution(int[] stones, int k) {
         int answer = 0;
         
-        int min = 0;
-        int max = 200_000_000;
+        int left = 0;
+        int right = 200_000_000;
         
-        while(min <= max) {
-            int mid = (min+max) / 2;
-            if(isPossible(mid, stones, k)) {
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            
+            if(isPossible(mid, stones.clone(), k)) {
                 answer = Math.max(answer, mid);
-                min = mid+1;
+                left = mid + 1;
             } else {
-                max = mid-1;
+                right = mid - 1;
             }
         }
         
         return answer;
     }
     
-    public boolean isPossible(int mid, int[] stones, int k) {
+    public boolean isPossible(int num, int[] stones, int k) {
         int cnt = 0;
         for(int i=0; i<stones.length; i++) {
-            if(mid > stones[i]) cnt++;
-            else cnt = 0;
-            
+            if(stones[i] < num) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
             if(cnt == k) return false;
         }
+        
         return true;
     }
 }
