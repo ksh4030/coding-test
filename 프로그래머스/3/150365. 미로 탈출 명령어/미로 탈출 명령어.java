2a276a4d6633cc0e40821d4db1ba5e7d828ccad9
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-10 20:37:51 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 7.85 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,42 +1,36 @@
 import java.util.*;
 class Solution {
-    //dlru
     static int[] dr = {1, 0, 0, -1};
     static int[] dc = {0, -1, 1, 0};
     static String[] dist = {"d", "l", "r", "u"};
-    static String ans = "impossible";
-    static StringBuilder target = new StringBuilder();
-    
+    StringBuilder sb = new StringBuilder();
+    String ans = "impossible";
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
-        
         if((Math.abs(x-r) + Math.abs(y-c))%2 != k%2) return ans;
         dfs(0, k, new Node(x-1, y-1), new Node(r-1, c-1), n, m);
-        
-        answer = ans;
-        return answer;
+        return ans;
     }
     
-    public static void dfs(int depth, int k, Node cur, Node end, int n, int m) {
-        if(ans != "impossible") return;
-        if(depth + Math.abs(cur.r-end.r) + Math.abs(cur.c-end.c) > k) return;
-        if(depth == k) {
-            ans = target.toString();
+    public void dfs(int depth, int k, Node cur, Node end, int n, int m) {
+        if(!ans.equals("impossible")) return;
+        if(depth + Math.abs(cur.r - end.r) + Math.abs(cur.c - end.c) > k) return;
+        if(depth == k && cur.r == end.r && cur.c == end.c) {
+            ans = sb.toString();
             return;
         }
         
         for(int i=0; i<4; i++) {
-            int nr = dr[i]+cur.r;
-            int nc = dc[i]+cur.c;
+            int nr = dr[i] + cur.r;
+            int nc = dc[i] + cur.c;
             
             if(nr<0 || nc<0 || nr>=n || nc>=m) continue;
-            target.append(dist[i]);
+            sb.append(dist[i]);
             dfs(depth+1, k, new Node(nr, nc), end, n, m);
-            target.delete(depth, depth+1);
+            sb.delete(depth, depth+1);
         }
     }
     
-    static class Node {
+    class Node {
         int r, c;
         public Node(int r, int c) {
             this.r = r;
