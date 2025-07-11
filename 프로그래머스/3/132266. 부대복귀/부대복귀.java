8fc936d8baf0e16ad68c13d97707dd6ac7fd05b6
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-11 21:09:13 +0900

    [level 3] Title: 부대복귀, Time: 150.06 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -2,37 +2,21 @@ import java.util.*;
 class Solution {
     static List<Integer>[] map;
     static int[] dist;
-    
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         int[] answer = new int[sources.length];
         init(n, roads);
+        bfs(n, destination);
         
         for(int i=0; i<sources.length; i++) {
-            bfs(n, destination);
+            answer[i] = dist[sources[i]];
         }
-        getAns(answer, sources);
+        
         return answer;
-    }
-    
-    public void init(int n, int[][] roads) {
-        map = new ArrayList[n+1];
-        for(int i=0; i<n+1; i++) {
-            map[i] = new ArrayList<>();
-        }
-        
-        for(int[] r : roads) {
-            map[r[0]].add(r[1]);
-            map[r[1]].add(r[0]);
-        }
-        
-        dist = new int[n + 1];
-        Arrays.fill(dist, -1);
     }
     
     public void bfs(int n, int destination) {
         Queue<Integer> q = new LinkedList<>();
         q.add(destination);
-        
         dist[destination] = 0;
         
         while(!q.isEmpty()) {
@@ -46,10 +30,19 @@ class Solution {
         }
     }
     
-    public void getAns(int[] answer, int[] sources) {
-        for(int i=0; i<sources.length; i++) {
-            answer[i] = dist[sources[i]];
+    public void init(int n, int[][] roads) {
+        map = new ArrayList[n+1];
+        
+        for(int i=0; i<=n; i++) {
+            map[i] = new ArrayList<>();
         }
+        
+        for(int[] arr : roads) {
+            map[arr[0]].add(arr[1]);
+            map[arr[1]].add(arr[0]);
+        }
+        
+        dist = new int[n+1];
+        Arrays.fill(dist, -1);
     }
-    
 }
