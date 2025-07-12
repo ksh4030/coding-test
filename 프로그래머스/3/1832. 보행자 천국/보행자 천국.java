6f6048293f043c0bb882faf4c622001ec6754cf3
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-12 23:03:03 +0900

    [level 3] Title: 보행자 천국, Time: 143.89 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,27 +1,28 @@
 import java.util.*;
 class Solution {
-    static int MOD = 20170805;
+    int MOD = 20170805;
+    static int[][][] dp;
     public int solution(int m, int n, int[][] cityMap) {
         int answer = 0;
-        
-        int[][][] dp = new int[m+1][n+1][2];
+        dp = new int[m+1][n+1][2]; //down = 0, left to right = 1
         dp[1][1][0] = 1;
         dp[1][1][1] = 1;
         
         for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
                 if(cityMap[i-1][j-1] == 0) {
-                    dp[i][j][0] += ((dp[i-1][j][0] + dp[i][j-1][1]) % MOD);
-                    dp[i][j][1] += ((dp[i-1][j][0] + dp[i][j-1][1]) % MOD);
+                    dp[i][j][0] += (dp[i-1][j][0] + dp[i][j-1][1]) % MOD;
+                    dp[i][j][1] += (dp[i-1][j][0] + dp[i][j-1][1]) % MOD;
                 } else if (cityMap[i-1][j-1] == 1) {
                     dp[i][j][0] = 0;
-                    dp[i][j][1] = 0;
+                    dp[i][j][1] = 0;                    
                 } else {
-                    dp[i][j][0] = dp[i-1][j][0];
-                    dp[i][j][1] = dp[i][j-1][1];
+                    dp[i][j][0] += dp[i-1][j][0];
+                    dp[i][j][1] += dp[i][j-1][1];
                 }
             }
-        }        
+        }
+        
         return dp[m][n][0];
     }
 }
