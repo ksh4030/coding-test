bcbb724064d8398c04a1ef514e4bfe439418e6e8
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-18 20:56:28 +0900

    [level 2] Title: 리코쳇 로봇, Time: 9.55 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,65 +1,69 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, -1, 0, 1};
     static String[][] map;
-    static int ans = -1;
-    static boolean[][] v;
+    static Node start, end;
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
+    static boolean[][][] v;
     public int solution(String[] board) {
-        int answer = 0;
-        map = new String[board.length][board[0].length()];
-        v = new boolean[board.length][board[0].length()];
-        
-        for(int i=0; i<map.length; i++) map[i] = board[i].split("");
-        
-        for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[0].length; j++) {
-                if(map[i][j].equals("R")) {
-                    bfs(new Node(i, j, 0));
-                }
-            }
-        }
-        
-        return ans;
+        init(board);
+        return bfs();
     }
     
-    public void bfs(Node node) {
+    public int bfs() {
         Queue<Node> q = new LinkedList<>();
-        q.add(node);
+        q.add(new Node(start.r, start.c, 0, -1));
         
         while(!q.isEmpty()) {
             Node cur = q.poll();
-            v[cur.r][cur.c] = true;
-            if(map[cur.r][cur.c].equals("G")) {
-                ans = cur.cnt;
-                return;
+            if(cur.r == end.r && cur.c == end.c) {
+                return cur.cnt;
             }
             
             for(int i=0; i<4; i++) {
-                int nr = cur.r;
-                int nc = cur.c;
+                if(i == cur.dir) continue;
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
                 
                 while(true) {
+                    if(nr < 0 || nc<0 || nr>=map.length || nc >= map[0].length || map[nr][nc].equals("D")) {
+                        nr -= dr[i];
+                        nc -= dc[i];
+                        break;
+                    }
                     nr += dr[i];
                     nc += dc[i];
-                    if(nr < 0 || nc < 0 || nr>=map.length || nc>=map[0].length || map[nr][nc].equals("D")) break;    
                 }
                 
-                nr -= dr[i];
-                nc -= dc[i];
-                
-                if(v[nr][nc]) continue;
-                q.add(new Node(nr, nc, cur.cnt+1));
+                if(cur.r == nr && cur.c == nc) continue;
+                if(!v[nr][nc][i]) {
+                    v[nr][nc][i] = true;
+                    q.add(new Node(nr, nc, cur.cnt+1, i));
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public void init(String[] board) {
+        map = new String[board.length][board[0].length()];
+        v = new boolean[board.length][board[0].length()][4];
+        for(int i=0; i<board.length; i++) {
+            for(int j=0; j<board[i].length(); j++) {
+                if(board[i].charAt(j) == 'R') start = new Node(i,j,0,0);
+                if(board[i].charAt(j) == 'G') end = new Node(i,j,0,0);
+                map[i][j] = String.valueOf(board[i].charAt(j));
             }
         }
     }
     
-    class Node {
-        int r, c, cnt;
-        public Node(int r, int c, int cnt) {
+    static class Node {
+        int r, c, cnt, dir;
+        public Node(int r, int c, int cnt, int dir) {
             this.r = r;
             this.c = c;
             this.cnt = cnt;
+            this.dir = dir;
         }
     }
 }
