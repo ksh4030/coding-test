fd3acc4f3379720d4eca290e48bc5c0a85fe3f32
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-25 16:07:55 +0900

    [level 3] Title: 풍선 터트리기, Time: 41.77 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -1,33 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] a) {
-        int n = a.length;
-        if (n < 3) return n;
+        int answer = 2; //양 끝은 무조건 남을 수 있음
+        int[] right = new int[a.length], left = new int[a.length];
+        left[0] = a[0];
+        right[a.length-1] = a[a.length-1];
         
-        int[] leftMin = new int[n];
-        int[] rightMin = new int[n];
-        
-        leftMin[0] = a[0];
-        rightMin[n-1] = a[n-1];
-        
-        // 왼쪽에서 오른쪽으로 최소값 저장
-        for (int i = 1; i < n; i++) {
-            leftMin[i] = Math.min(leftMin[i-1], a[i]);
+        for(int i=1; i<a.length; i++) {
+            left[i] = Math.min(left[i-1], a[i]);
         }
         
-        // 오른쪽에서 왼쪽으로 최소값 저장
-        for (int i = n-2; i >= 0; i--) {
-            rightMin[i] = Math.min(rightMin[i+1], a[i]);
+        for(int i=a.length-2; i>=0; i--) {
+            right[i] = Math.min(right[i+1], a[i]);
         }
         
-        int answer = 2; // 양 끝의 풍선은 남을 수 있음
-        
-        // 중간의 풍선이 남을 수 있는지 확인
-        for (int i = 1; i < n-1; i++) {
-            if (a[i] <= leftMin[i-1] || a[i] <= rightMin[i+1]) {
-                answer++;
-            }
+        for(int i=1; i<a.length-1; i++) {
+            if(a[i] <= left[i-1] || a[i] <= right[i+1]) answer++;
         }
         
         return answer;
