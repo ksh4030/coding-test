0cbc76c0dd883916d429c927b5dc6a0edd188e0d
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-07-31 14:48:40 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.09 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,20 +1,15 @@
 import java.util.*;
-
 class Solution {
     public long solution(int w, int h) {
-        long total = (long) w * h; // 전체 사각형 개수
-        long gcd = gcd(w, h);     // 최대공약수 계산
-        long unusable = w + h - gcd; // 대각선이 지나가는 칸의 수
-        return total - unusable; // 사용 가능한 사각형의 개수
+        return (long)w*h - (w+h - gcd(w,h));
     }
     
-    // 최대공약수 계산 (유클리드 호제법)
-    private long gcd(int a, int b) {
-        while (b != 0) {
-            int temp = a % b;
-            a = b;
-            b = temp;
+    public long gcd(long w, long h) {
+        while(h != 0) {
+            long tmp = w % h;
+            w = h;
+            h = tmp;
         }
-        return a;
+        return w;
     }
 }
