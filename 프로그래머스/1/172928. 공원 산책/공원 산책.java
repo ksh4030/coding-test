cd9988792762b4093d5a2429c66184ea0a6effe4
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-08-12 22:49:06 +0900

    [level 1] Title: 공원 산책, Time: 5.83 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,56 +1,28 @@
+import java.util.*;
 class Solution {
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static String[][] map;
+    static Node cur;
+    
+    static int[] dr = {0,0,1,-1}; //E W S N
+    static int[] dc = {1,-1,0,0};
     
     public int[] solution(String[] park, String[] routes) {
         int[] answer = new int[2];
-        String[][] map = new String[park.length][park[0].length()];
-        int R = map.length;
-        int C = map[0].length;
-        Node cur = new Node(-1, -1);
+        init(park);
         
-        for(int i=0; i<park.length; i++) {
-            map[i] = park[i].split("");
-        }
-        
-        loop:
-        for(int i=0; i<map.length; i++) {
-            for(int j=0; j<map[0].length; j++) {
-                if(map[i][j].equals("S")) {
-                    cur = new Node(i,j);
-                    break loop;
-                }
-            }
-        }
-        
-        for(int i=0; i<routes.length; i++) {
-            String[] bucket = routes[i].split(" ");
-            
-            int dist = -1;            
-            switch(bucket[0]){
-                case "N" :
-                    dist = 0;
-                    break;
-                case "S" :
-                    dist = 1;
-                    break;
-                case "W" :
-                    dist = 2;
-                    break;
-                case "E" :
-                    dist = 3;
-                    break;
-            }
+        for(String s : routes) {
+            boolean flag = true;
+            String[] arr = s.split(" ");
+            int dist = getDist(arr[0]);
+            int n = Integer.parseInt(arr[1]);
             
             int nr = cur.r;
             int nc = cur.c;
-            boolean flag = true;
             
-            for(int j=0; j<Integer.parseInt(bucket[1]); j++) {
+            for(int i=0; i<n; i++) {
                 nr += dr[dist];
                 nc += dc[dist];
-                
-                if(nr>=R || nc>=C || nr<0 || nc<0 || map[nr][nc].equals("X")) {
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || map[nr][nc].equals("X")) {
                     flag = false;
                     break;
                 }
@@ -60,16 +32,46 @@ class Solution {
                 cur = new Node(nr, nc);
             }
         }
+        
         answer[0] = cur.r;
         answer[1] = cur.c;
         return answer;
     }
     
-    public class Node {
+    public int getDist(String s) {
+        switch(s) {
+            case "E" :
+                return 0;
+            case "W" :
+                return 1;
+            case "S" :
+                return 2;
+            case "N" :
+                return 3;                
+        }
+        return -1;
+    }
+    
+    public void init(String[] park) {
+        map = new String[park.length][park[0].length()];
+        for(int i=0; i<map.length; i++) {
+            map[i] = park[i].split("");
+            for(int j=0; j<map[i].length; j++) {
+                if(map[i][j].equals("S")) cur = new Node(i, j);
+            }
+        }
+    }
+    
+    static class Node {
         int r, c;
-        public Node(int r, int c){
+        public Node(int r, int c) {
             this.r = r;
             this.c = c;
+        }
+        
+        @Override
+        public String toString() {
+            return r + " " + c;
         }
     }
 }
