7dd261d34851fad55e7acebf117e9cdb09a20c4d
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-08-13 20:49:44 +0900

    [level 1] Title: 달리기 경주, Time: 388.06 ms, Memory: 385 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -2,32 +2,29 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         String[] answer = new String[players.length];
-        Map<String, Integer> man = new HashMap<>();
-        Map<Integer, String> rank = new HashMap<>();
         
-        int cnt = 1;
+        HashMap<String, Integer> player = new HashMap<>();
+        HashMap<Integer, String> grade = new HashMap<>();
+        
         for(int i=0; i<players.length; i++) {
-            man.put(players[i], cnt);
-            rank.put(cnt, players[i]);
-            cnt++;
+            player.put(players[i], i);
+            grade.put(i, players[i]);
+        } 
+        
+        for(String s : callings) {
+            int g = player.get(s);
+            String cur = grade.get(g);
+            String next = grade.get(g-1);
+            
+            players[g-1] = cur;
+            players[g] = next;
+            
+            player.put(next, g);
+            player.put(cur, g-1);
+            grade.put(g, next);
+            grade.put(g-1, cur);
         }
         
-        for(int i=0; i<callings.length; i++) {
-            String cur = callings[i];
-            int curRank = man.get(cur);
-            
-            String target = rank.get(curRank-1);
-            int targetRank = curRank-1;
-            
-            man.put(cur, targetRank);
-            man.put(target, curRank);
-            rank.put(targetRank, cur);
-            rank.put(curRank, target);
-        }
-        
-        for(int i=1; i<=cnt-1; i++) {
-            answer[i-1] = rank.get(i);
-        }
-        return answer;
+        return players;
     }
 }
