f89389f008958a201186543b86af4dd5be55a271
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-08-14 16:44:33 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.34 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,17 +1,16 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] wallpaper) {
         int[] answer = new int[4];
+        
         answer[0] = Integer.MAX_VALUE;
         answer[1] = Integer.MAX_VALUE;
-        answer[2] = Integer.MIN_VALUE;
-        answer[3] = Integer.MIN_VALUE;
+        answer[2] = 0;
+        answer[3] = 0;
         
         for(int i=0; i<wallpaper.length; i++) {
-            String[] arr = wallpaper[i].split("");
-            for(int j=0; j<arr.length; j++) {
-                if(arr[j].equals("#")) {
+            for(int j=0; j<wallpaper[0].length(); j++) {
+                if(wallpaper[i].charAt(j) == '#') {
                     answer[0] = Math.min(answer[0], i);
                     answer[1] = Math.min(answer[1], j);
                     answer[2] = Math.max(answer[2], i+1);
