270251b26a714912f388fbb11b8273e264a7dc6f
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-09-02 16:12:53 +0900

    [Gold V] Title: 토마토, Time: 588 ms, Memory: 120564 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,86 +1,63 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N, M;
-	static int[][] arr;
-	static int[] dr = {-1, 0, 1, 0};
-	static int[] dc = {0, 1, 0, -1};
-	static Queue<Node> tomato = new LinkedList<>();
-	static int answer;
+    static int[][] map;
+    static Queue<Node> q = new LinkedList<>();
+    static int answer = 0;
     public static void main(String[] args) throws IOException {
-    	
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    	
-    	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    	
-    	M = Integer.parseInt(st.nextToken());
-    	N = Integer.parseInt(st.nextToken());
-    	
-    	arr = new int[N][M];
-    	answer = 0;
-    	
-    	for(int i=0; i<N; i++) {
-    		st = new StringTokenizer(br.readLine(), " ");
-    		for(int j=0; j<M; j++) {
-    			arr[i][j] = Integer.parseInt(st.nextToken());
-    			if(arr[i][j] == 1) {
-    				arr[i][j] = 2;
-    				tomato.add(new Node(i,j,0));
-    			}
-    		}
-    	}
-    	bfs();
-    	loop:
-    	for(int i=0; i<N; i++) {
-    		for(int j=0; j<M; j++) {
-    			if(arr[i][j] == 0) {
-    				answer = -1;
-    				break loop;
-    			}
-    		}
-    	}
-    	System.out.println(answer);
-    	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        map = new int[C][R];
+
+        for(int i=0; i<C; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<R; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1) q.add(new Node(i, j, 0));
+            }
+        }
+        bfs();
+        System.out.println(isPossible() ? answer : -1);
     }
-    
+
     public static void bfs() {
-    	
-    	while(tomato.size() > 0) {
-    		Node current = tomato.poll();
-    		
-    		for(int i=0; i<4; i++) {
-    			int nr = current.r + dr[i];
-    			int nc = current.c + dc[i];
-    			
-    			if(nr < 0 || nc < 0 || nr >= N || nc >= M || arr[nr][nc] != 0) {
-    				continue;
-    			}
-    			if(arr[nr][nc] == 0) {
-    				arr[nr][nc] = 2;
-    				tomato.add(new Node(nr, nc, current.cnt+1));
-    				answer = Math.max(answer, current.cnt + 1);
-    			}
-    		}
-    		
-    	}
-    	
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            answer = cur.day;
+
+            for(int i=0; i<4; i++) {
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
+
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || map[nr][nc] != 0) continue;
+                q.add(new Node(nr, nc, cur.day+1));
+                map[nr][nc] = 1;
+            }
+        }
     }
     
-    static class Node{
-    	int r;
-    	int c;
-    	int cnt;
-		public Node(int r, int c, int cnt) {
-			super();
-			this.r = r;
-			this.c = c;
-			this.cnt = cnt;
-		}
+    public static boolean isPossible() {
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[0].length; j++) {
+                if(map[i][j] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    static class Node {
+        int r, c, day;
+        public Node(int r, int c, int day) {
+            this.r = r;
+            this.c = c;
+            this.day = day;
+        }
     }
 }
-
-
-
