3b103a34b967ad1a1557e8dca443c39711ac4cb3
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-09-09 16:31:30 +0900

    [Silver IV] Title: ATM, Time: 272 ms, Memory: 20080 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int answer = 0;
+
+        int[] arr = new int[N];
+        int[] nArr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        nArr[0] = arr[0];
+
+        for (int i = 1; i < N; i++) {
+            nArr[i] = nArr[i - 1] + arr[i];
+        }
+
+        for(int n : nArr) {
+            answer += n;
+        }
+
+        System.out.println(answer);
+
+    }
+}
