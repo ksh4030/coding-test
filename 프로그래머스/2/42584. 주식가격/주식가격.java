9055435a39e6930e750c720cbb351ead983fa34d
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-09-18 17:45:24 +0900

    [level 2] Title: 주식가격, Time: 24.51 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,22 +1,13 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        
-        Stack<Integer> stack = new Stack<>();
-        for(int i=0; i<prices.length; i++) {
-            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int n = stack.pop();
-                answer[n] = i - n;
-            }            
-            stack.push(i);
+
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i+1; j < prices.length; j++) {
+                answer[i]++;
+                if(prices[i] > prices[j]) break;
+            }
         }
-        
-        while(!stack.isEmpty()) {
-            int n = stack.pop();
-            answer[n] = prices.length - 1 - n;
-        }
-        
         return answer;
     }
 }
