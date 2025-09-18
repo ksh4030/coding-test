2a75c26d969a0a5507e8d5c52076c9d2b9fdb029
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-09-18 16:21:48 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,26 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static final int MOD = 10_007;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
-        if(N==1) {
-            System.out.println(1);
-            return;
-        } else if (N==2) {
-            System.out.println(2);
-            return;
-        }
+        int[] arr = new int[N+1];
+        arr[1] = 1;
+        if(N>1) arr[2] = 2;
 
-        long[] arr = new long[N+1];
-        arr[1]=1;
-        arr[2]=2;
-        for(int i=3; i<=N; i++) {
-            arr[i] = (arr[i-1] + arr[i-2])  % 10_007;
+        for (int i = 3; i <= N; i++) {
+            arr[i] = (arr[i-2] + arr[i-1]) % MOD;
         }
 
         System.out.println(arr[N]);
