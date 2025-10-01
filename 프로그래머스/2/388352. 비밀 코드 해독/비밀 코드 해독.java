1e9fccbecc9d93e98a348ea3e065024be941f8e2
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-10-02 02:00:58 +0900

    [level 2] Title: 비밀 코드 해독, Time: 38.24 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,31 +1,37 @@
 import java.util.*;
 class Solution {
     static int answer = 0;
-    public int solution(int n, int[][] q, int[] ans) {        
-        comb(1, 5, n, new ArrayList<>(), q, ans);
+    static int[][] qArr;
+    static int[] ansArr;
+    public int solution(int n, int[][] q, int[] ans) {
+        qArr = q;
+        ansArr = ans;
+        
+        comb(1, n, new ArrayList<>());
+        
         return answer;
     }
     
-    public void comb(int cur, int r, int n, List<Integer> list, int[][] q, int[] ans) {
-        if(list.size() == r) {
-            if(isPossible(list, q, ans)) answer++;
+    public void comb(int cur, int n, List<Integer> list) {
+        if(list.size() == 5) {
+            if(isPossible(list)) answer++;
             return;
         }
         
         for(int i=cur; i<=n; i++) {
             list.add(i);
-            comb(i+1, r, n, list, q, ans);
-            list.remove(list.size()-1);
+            comb(i+1, n, list);
+            list.remove(list.size() - 1);
         }
     }
     
-    public boolean isPossible(List<Integer> list, int[][] q, int[] ans) {
-        for(int i=0; i<q.length; i++) {
+    public boolean isPossible(List<Integer> list) {
+        for(int i=0; i<qArr.length; i++) {
             int cnt = 0;
-            for(int j=0; j<q[i].length; j++) {
-                if(list.contains(q[i][j])) cnt++;
+            for(int j=0; j<qArr[i].length; j++) {
+                if(list.contains(qArr[i][j])) cnt++;
             }
-            if(ans[i] != cnt) return false;
+            if(ansArr[i] != cnt) return false;
         }
         return true;
     }
