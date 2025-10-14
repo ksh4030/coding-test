4c179ee52c7e4e48fd51a01754e6442a977e5cf1
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-10-14 10:01:47 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.61 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -5,8 +5,7 @@ class Solution {
         int answer = 0;
         
         int[] arr = new int[n];
-        arr[0] = 1;
-        arr[1] = 2;
+        arr[0] = 1, arr[1] = 2;
         
         for (int i = 2; i < n; i++) arr[i] = (arr[i - 1] + arr[i - 2]) % MOD;
         
