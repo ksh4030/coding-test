69f7fac67d1e3c9a0b398877b5f768208b942a9b
김수환 <124498333+ksh4030@users.noreply.github.com>
2025-10-15 01:14:05 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -1,22 +1,16 @@
 class Solution {
-    private static long totalUpTo(long T) {
-        // 0초부터 T초까지(포함) 알람 총 횟수
-        long sm = (T * 59) / 3600 + 1;      // 초-분
-        long sh = (T * 719) / 43200 + 1;    // 초-시
-        long all = (T / 43200) + 1;         // 세 바늘 동시 (중복 제거용)
-        return sm + sh - all;
-    }
-
-    private static int startHits(long T) {
-        // 시작 시각에 바로 울리면 +1 (정각마다 초-분 겹침)
-        return (T % 3600 == 0) ? 1 : 0;
-    }
-
     public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
-        long t1 = h1 * 3600L + m1 * 60L + s1;
-        long t2 = h2 * 3600L + m2 * 60L + s2;
+        long time1 = (h1 * 3600) + (m1 * 60) + s1;
+        long time2 = (h2 * 3600) + (m2 * 60) + s2;
 
-        long ans = totalUpTo(t2) - totalUpTo(t1) + startHits(t1);
-        return (int) ans;
+        return (int) (cntAlarm(time2) - cntAlarm(time1) + (time1 % 3600 == 0 ? 1 : 0));
+    }
+    
+    public long cntAlarm(long time) {
+        long secMin = (time * 59) / 3600 + 1;
+        long secHour = (time * 719) / 43200 + 1;
+        long secMinHour = (time / 43200) + 1;
+        
+        return secMin + secHour - secMinHour;
     }
 }
