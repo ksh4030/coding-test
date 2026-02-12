7700921322dc969ebc643b62e20506fde5fe259e
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-02-12 23:50:42 +0900

    [level 2] Title: 피보나치 수, Time: 4.05 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        final int MOD = 1234567;
-        
-        int[] arr = new int[n+1];
-        arr[0] = 0;
+        int answer = 0;        
+        int[] arr = new int[n];
+        arr[0] = 1;
         arr[1] = 1;
         
-        for(int i=2; i<=n; i++) {
-            arr[i] = (arr[i-2] + arr[i-1]) % MOD;
+        for(int i=2; i<n; i++) {
+            int x = arr[i-1] + arr[i-2];
+            arr[i] = x%1234567;
         }
         
-        return arr[arr.length-1];
+        return arr[n-1];
     }
 }
