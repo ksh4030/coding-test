3f12244c49cd6bab58d36041cc031158c203f780
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-02-12 22:24:30 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
         int sum = brown + yellow;
-
-        for (int i = 3; i < sum; i++) {
-            int n = sum / i;
-
-            if(n >= 3 && sum % i == 0) {
-                answer[0] = Math.max(i, n);
-                answer[1] = Math.min(i, n);
-                int y = (answer[0] - 2) * (answer[1] - 2);
-
-                if(y == yellow) {
-                    return answer;
-                }
-            }
+        
+        for(int i=3; i<sum; i++) {
+            if(sum % i != 0) continue;
+            int n = sum/i;
+            answer[0] = Math.max(i, n);
+            answer[1] = Math.min(i, n);
+            
+            if((answer[0]-2) * (answer[1]-2) == yellow) return answer;
         }
         
         return answer;
