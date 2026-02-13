617200c66109ff0326c260b3be18ed5b94e2f233
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-02-13 23:17:38 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,6 @@
-select a.animal_id, a.name
-from animal_ins a, animal_outs b
-where a.animal_id = b.animal_id and b.datetime < a.datetime
-order by a.datetime
+-- 코드를 입력하세요
+SELECT a.animal_id, a.name
+from animal_ins a
+join animal_outs b on a.animal_id = b.animal_id
+where a.datetime > b.datetime
+order by a.datetime;
