5b6e25689e1a44c909c161273aa588d7f987e970
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-02-28 16:32:27 +0900

    [level 2] Title: 무인도 여행, Time: 13.50 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,16 +1,14 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    
-    static List<Integer> list = new ArrayList<>();
-    static boolean[][] v;
     static int[][] map;
-    public int[] solution(String[] maps) {        
+    static boolean[][] v;
+    public int[] solution(String[] maps) {
+        List<Integer> list = new ArrayList<>();        
+        
         init(maps);
         
-        for(int i=0; i<maps.length; i++) {
-            for(int j=0; j<map[i].length; j++) {
+        for(int i=0; i<map.length; i++) {
+            for(int j=0; j<map[0].length; j++) {
                 if(map[i][j] > 0 && !v[i][j]) {
                     list.add(bfs(i, j));
                 }
@@ -19,59 +17,54 @@ class Solution {
         
         if(list.size() == 0) return new int[]{-1};
         
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = list.get(i);
+        }
         Arrays.sort(answer);
-        
         return answer;
     }
     
     public int bfs(int r, int c) {
         Queue<Node> q = new LinkedList<>();
         q.add(new Node(r, c));
+        int sum = 0;
         v[r][c] = true;
         
-        int sum = map[r][c];
+        
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
         
         while(!q.isEmpty()) {
             Node cur = q.poll();
+            sum += map[cur.r][cur.c];            
             
             for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
                 
-                if(isPossible(nr, nc)) {
-                    v[nr][nc] = true;
-                    q.add(new Node(nr, nc));
-                    sum += map[nr][nc];
-                }
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || v[nr][nc] || map[nr][nc] == 0) continue;
+                q.add(new Node(nr, nc));
+                v[nr][nc] = true;
             }
         }
         
         return sum;
     }
     
-    public boolean isPossible(int r, int c) {
-        if(r<0 || c<0 || r>=map.length || c>=map[0].length || map[r][c]==0 || v[r][c]) return false;
-        return true;
-    }
-    
     public void init(String[] maps) {
-        int r = maps.length;
-        int c = maps[0].length();
-        
-        map = new int[r][c];
-        v = new boolean[r][c];
+        map = new int[maps.length][maps[0].length()];
+        v = new boolean[maps.length][maps[0].length()];
         
         for(int i=0; i<maps.length; i++) {
-            for(int j=0; j<maps[0].length(); j++) {
-                if(maps[i].charAt(j) != 'X') {
-                    map[i][j] = maps[i].charAt(j) - '0';
-                }
+            String[] bucket = maps[i].split("");
+            for(int j=0; j<bucket.length; j++) {
+                map[i][j] = bucket[j].equals("X") ? 0 : Integer.parseInt(bucket[j]);
             }
         }
     }
     
-    class Node {
+    static class Node {
         int r, c;
         public Node(int r, int c) {
             this.r = r;
