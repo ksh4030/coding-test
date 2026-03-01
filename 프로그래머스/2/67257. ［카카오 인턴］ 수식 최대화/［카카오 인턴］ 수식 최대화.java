e5fd591366735cec3b61f592b11819e27011f8f6
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-03-02 02:30:49 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 3.43 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,73 +1,67 @@
 import java.util.*;
 class Solution {
-    static String[][] cases = {
-        {"+","-","*"},
-        {"+","*","-"},
-        {"*","-","+"},
-        {"*","+","-"},
-        {"-","+","*"},
-        {"-","*","+"}
+    static String[][] operator = {
+        {"*", "-", "+"},
+        {"*", "+", "-"},
+        {"-", "*", "+"},
+        {"-", "+", "*"},
+        {"+", "-", "*"},
+        {"+", "*", "-"},
     };
-    
-    static List<Long> num = new ArrayList<>();
-    static List<String> oper = new ArrayList<>();
-    
+    static List<String> list = new ArrayList<>();
     public long solution(String expression) {
+        long answer = Integer.MIN_VALUE;
+        
         init(expression);
-        return getMax();
-    }
-    
-    public long getMax() {
-        long max = 0;
-        for(int i=0; i<cases.length; i++) {
-            List<Long> n = new ArrayList<>();
-            n.addAll(num);
-            List<String> o = new ArrayList<>();
-            o.addAll(oper);
+        
+        for(int i=0; i<operator.length; i++) {
+            List<String> bucket = new ArrayList<>(list);
             
-            for(int j=0; j<cases[i].length; j++) {
-                for(int k=0; k<o.size(); k++) {
-                    if(o.get(k).equals(cases[i][j])) {
-                        long bucket = cal(n.get(k), n.get(k+1), cases[i][j]);
-                        n.remove(k+1);
-                        n.remove(k);
-                        o.remove(k);
-                        
-                        n.add(k, bucket);
+            for(int j=0; j<operator[i].length; j++) {
+                for(int k=0; k<bucket.size(); k++) {
+                    if(bucket.get(k).equals(operator[i][j])) {
+                        cal(operator[i][j], k, bucket);
                         k--;
                     }
                 }
             }
-            max = Math.max(max, Math.abs(n.get(0)));
+            
+            answer = Math.max(answer, Math.abs(Long.parseLong(bucket.get(0))));
         }
-        return max;
+        
+        return answer;
     }
     
-    public long cal(long a, long b, String c) {        
-        switch(c) {
-            case "*" :
-                return a * b;
-            case "+" :
-                return a + b;
-            case "-" :
-                return a - b;
+    public void cal(String oper, int idx, List<String> bucket) {
+        long x = Long.parseLong(bucket.get(idx-1));
+        long y = Long.parseLong(bucket.get(idx+1));
+        long num = 0;
+        
+        if(oper.equals("*")) {
+            num = x*y;
+        } else if (oper.equals("+")) {
+            num = x+y;
+        } else {
+            num = x-y;
         }
-        return -1;
+        
+        for(int i=0; i<3; i++) bucket.remove(idx-1);
+        bucket.add(idx-1, Long.toString(num));
     }
     
     public void init(String expression) {
+        String[] arr = expression.split("");
         String bucket = "";
-        for(int i=0; i<expression.length(); i++) {
-            char cur = expression.charAt(i);
-            
-            if(cur == '*' || cur == '-' || cur == '+') {
-                num.add(Long.parseLong(bucket));
-                oper.add(String.valueOf(cur));
+        
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i].equals("*") || arr[i].equals("+") || arr[i].equals("-")) {
+                list.add(bucket);
+                list.add(arr[i]);
                 bucket = "";
             } else {
-                bucket += String.valueOf(cur);
+                bucket += arr[i];
             }
         }
-        num.add(Long.parseLong(bucket));
+        list.add(bucket);
     }
 }
