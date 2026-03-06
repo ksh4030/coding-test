6e0d40cfc7dde5880f1ceb0dfd24510173cdff7a
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-03-06 16:49:06 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,18 @@
--- 코드를 작성해주세요
-select e.emp_no, e.emp_name, 
-case
-    when avg(g.score) >= 96 then 'S'
-    when avg(g.score) >= 90 then 'A'
-    when avg(g.score) >= 80 then 'B'
+select a.emp_no, a.emp_name, 
+case 
+    when sum(c.score) / 2 >= 96 then 'S'
+    when sum(c.score) / 2 >= 90 then 'A'
+    when sum(c.score) / 2 >= 80 then 'B'
     else 'C'
 end as grade,
 case
-    when avg(g.score) >= 96 then e.sal * 0.2
-    when avg(g.score) >= 90 then e.sal * 0.15
-    when avg(g.score) >= 80 then e.sal * 0.1
+    when sum(c.score) / 2 >= 96 then a.sal * 0.2
+    when sum(c.score) / 2 >= 90 then a.sal * 0.15
+    when sum(c.score) / 2 >= 80 then a.sal * 0.1
     else 0
 end as bonus
-from hr_employees e
-join hr_grade g on e.emp_no = g.emp_no
-group by e.emp_no, e.emp_name
-order by e.emp_no;
+from HR_EMPLOYEES a
+join HR_DEPARTMENT b on a.dept_id = b.dept_id
+join HR_GRADE c on a.emp_no = c.emp_no
+group by a.emp_no, a.emp_name
+order by emp_no
