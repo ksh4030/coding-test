573d015028e239e281818c1049410e41f86b640d
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-03-07 15:59:41 +0900

    [level 3] Title: 등굣길, Time: 1.50 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,23 +1,36 @@
 import java.util.*;
 class Solution {
     static final int MOD = 1_000_000_007;
+    static int[][] map;
+    
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        int[][] dp = new int[n+1][m+1];
-        dp[0][1] = 1;
+        init(m,n,puddles);
         
-        for(int[] arr : puddles) dp[arr[1]][arr[0]] = -1;
+        map[1][1] = 1;
         
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                if(dp[i][j] == -1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
+        for(int i=1; i<map.length; i++) {
+            for(int j=1; j<map[0].length; j++) {
+                if(map[i][j] == -1) {
+                    continue;
+                } 
+                if(map[i-1][j] > 0) {
+                    map[i][j] += map[i-1][j] % MOD;
+                } 
+                if(map[i][j-1] > 0) {
+                    map[i][j] += map[i][j-1] % MOD;
                 }
             }
         }
         
-        return dp[n][m];
+        return map[n][m]%MOD;
+    }
+    
+    public void init(int m, int n, int[][] puddles) {
+        map = new int[n+1][m+1];
+        
+        for(int[] arr : puddles) {
+            map[arr[1]][arr[0]] = -1;
+        }
     }
 }
