e601b61c729d5844b626edea4fd304dd08e97bfc
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-04-04 02:08:29 +0900

    [level 3] Title: 최고의 집합, Time: 1.61 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -1,16 +1,16 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, int s) {
-        if (n > s) return new int[]{-1};
         int[] answer = new int[n];
-        int base = s / n;
-        int extra = s % n;
-        Arrays.fill(answer, base);
+        if(n > s) return new int[]{-1};        
+        Arrays.fill(answer, s / n);
         
-        for(int i=n-1; i>=n-extra; i--) {
-            answer[i]++;
+        if(s%n > 0) {
+            for(int i=0; i<s%n; i++) {
+                answer[i]++;
+            }
         }
-        
+        Arrays.sort(answer);
         return answer;
     }
 }
