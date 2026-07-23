58e87c24031aa2ce45c4a1ad92f4277c5f00e692
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-07-23 15:08:38 +0900

    [level 2] Title: 올바른 괄호, Time: 3.82 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,14 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
-        String[] arr = s.split("");
-        Stack<String> stack = new Stack<>();
+        int cnt = 0;
         
-        for(String a : arr) {
-            if(a.equals("(")) {
-                stack.push("(");
-            } else {
-                if(stack.isEmpty()) return false;
-                stack.pop();
-            }
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i) == ')') cnt--;
+            else cnt++;
+            if(cnt < 0) return false;
         }
         
-        return stack.isEmpty();
+        return cnt == 0;
     }
 }
