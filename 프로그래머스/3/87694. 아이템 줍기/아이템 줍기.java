2e1b32f83e4c1170228e088a9e1d6a3295ed194a
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-07-24 17:18:04 +0900

    [level 3] Title: 아이템 줍기, Time: 0.92 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,64 +1,63 @@
 import java.util.*;
-
 class Solution {
     static int[][] map = new int[101][101];
     static boolean[][] v = new boolean[101][101];
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, -1, 0, 1};
+    
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
+        int answer = 0;        
         characterX *= 2;
         characterY *= 2;
         itemX *= 2;
         itemY *= 2;
         
-        for(int[] arr : rectangle) {
-            for(int i=0; i<arr.length; i++) arr[i] *= 2;
-            makeMap(arr);
-        }
+        makeMap(rectangle);        
         
-        answer = bfs(new Node(characterX, characterY, 0), new Node(itemX, itemY, 0));        
-        return answer/2;
+        return bfs(characterX, characterY, itemX, itemY)/2;
     }
     
-    static int bfs(Node start, Node end) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(start);
+    public int bfs(int characterX, int characterY, int itemX, int itemY) {
+        Queue<Node> q = new ArrayDeque<>();
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
         
-        while(!q.isEmpty()) {            
+        q.add(new Node(characterX, characterY, 0));
+        v[characterX][characterY] = true;
+        
+        while(!q.isEmpty()) {
             Node cur = q.poll();
-            v[cur.r][cur.c] = true;
-            
-            if(cur.r == end.r && cur.c == end.c) {
-                return cur.cnt;
-            }
+            if(cur.r == itemX && cur.c == itemY) return cur.cnt;
             
             for(int i=0; i<4; i++) {
-                int nr = dr[i] + cur.r;
-                int nc = dc[i] + cur.c;
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
                 
-                if(nr<0 || nc<0 || nr>=101 || nc>=101 || v[nr][nc] || map[nr][nc]!=2) continue;
+                if(nr<0 || nc<0 || nr>=map.length || nc>=map[0].length || map[nr][nc] != 1 || v[nr][nc]) continue;
                 q.add(new Node(nr, nc, cur.cnt+1));
                 v[nr][nc] = true;
             }
         }
-        return 0;
+        
+        return -1;
     }
     
-    static void makeMap(int[] arr) {
-        for(int i=arr[0]; i<=arr[2]; i++) {
-            for(int j=arr[1]; j<=arr[3]; j++) {
-                if(map[i][j] == 1) continue;
-                if(i==arr[0] || i==arr[2] || j==arr[1] || j==arr[3]) {
-                    map[i][j] = 2;
-                } else {
-                    map[i][j] = 1;
+    public void makeMap(int[][] rectangle) {
+        for(int[] arr : rectangle) {
+            for(int i=0; i<arr.length; i++) arr[i] *= 2;
+            
+            for(int i=arr[0]; i<=arr[2]; i++) {
+                for(int j=arr[1]; j<=arr[3]; j++) {                    
+                    if(map[i][j] == 2) continue;
+                    if(i==arr[0] || i==arr[2] || j==arr[1] || j==arr[3]) {
+                        map[i][j] = 1;
+                    } else {
+                        map[i][j] = 2;
+                    }
                 }
             }
         }
     }
     
-    static class Node {
+    class Node {
         int r, c, cnt;
         public Node(int r, int c, int cnt) {
             this.r = r;
