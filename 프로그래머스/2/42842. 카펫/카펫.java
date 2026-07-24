b06064dd1a6bfe32c591c3741aa807c256e6cf9d
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-07-24 20:33:17 +0900

    [level 2] Title: 카펫, Time: 4.05 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,15 +1,23 @@
+import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int sum = brown + yellow;
+        List<Integer> list = new ArrayList<>();
         
-        for(int i=3; i<sum; i++) {
-            if(sum % i != 0) continue;
-            int n = sum/i;
-            answer[0] = Math.max(i, n);
-            answer[1] = Math.min(i, n);
+        for(int i=3; i<sum/2; i++) if(sum % i == 0) list.add(i);
+        
+        int left = 0;
+        int right = list.size() - 1;
+        
+        while(left <= right) {
+            if((list.get(left)-2) * (list.get(right)-2) == yellow) {
+                answer[0] = list.get(right);
+                answer[1] = list.get(left);
+            }
             
-            if((answer[0]-2) * (answer[1]-2) == yellow) return answer;
+            left++;
+            right--;
         }
         
         return answer;
