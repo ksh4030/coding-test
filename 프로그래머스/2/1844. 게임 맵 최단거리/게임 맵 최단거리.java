506db81e26b963b520ce879333b3ea61e271c976
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-07-24 15:55:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.24 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,36 +1,34 @@
 import java.util.*;
 class Solution {
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};    
-    
+    static boolean[][] v;
     public int solution(int[][] maps) {
         int answer = 0;
+        v = new boolean[maps.length][maps[0].length];
         
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] v = new boolean[n][m];
-        
-        answer = bfs(n, m, v, maps);
-        
-        return answer;
+        return bfs(maps);
     }
     
-    public int bfs(int n, int m, boolean[][] v, int[][] maps) {
-        Queue<Pos> q = new LinkedList<>();
-        q.add(new Pos(0, 0, 1));
+    public int bfs(int[][] maps) {
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0,0,1));
         v[0][0] = true;
         
         while(!q.isEmpty()) {
-            Pos cur = q.poll();
-            if(cur.r == n-1 && cur.c == m-1) return cur.cnt;
+            Node cur = q.poll();
+            if(cur.r == maps.length-1 && cur.c == maps[0].length-1) {
+                return cur.cnt;
+            }
             
             for(int i=0; i<4; i++) {
                 int nr = cur.r + dr[i];
                 int nc = cur.c + dc[i];
                 
-                if(nr<0 || nc<0 || nr>=n || nc>=m || maps[nr][nc] == 0 || v[nr][nc]) continue;
+                if(nr<0 || nc<0 || nr>=maps.length || nc>=maps[0].length || maps[nr][nc]==0 || v[nr][nc]) continue;
                 
-                q.add(new Pos(nr, nc, cur.cnt+1));
+                q.add(new Node(nr, nc, cur.cnt+1));
                 v[nr][nc] = true;
             }
         }
@@ -38,9 +36,9 @@ class Solution {
         return -1;
     }
     
-    class Pos{
+    class Node {
         int r, c, cnt;
-        public Pos(int r, int c, int cnt) {
+        public Node(int r, int c, int cnt) {
             this.r = r;
             this.c = c;
             this.cnt = cnt;
