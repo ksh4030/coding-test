14231bc5739d47e50fc2fcd64333c2bb3899de7b
김수환 <124498333+ksh4030@users.noreply.github.com>
2026-07-24 16:17:23 +0900

    [level 3] Title: 단어 변환, Time: 0.91 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,53 @@
 import java.util.*;
 class Solution {
-    static int answer = Integer.MAX_VALUE;
     static boolean[] v;
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         v = new boolean[words.length];
-        dfs(begin, target, words, 0);
-        
-        return answer == Integer.MAX_VALUE ? 0 : answer;
+        return bfs(begin, target, words);
     }
     
-    public void dfs(String cur, String target, String[] words, int cnt) {
-        if(cur.equals(target)) {
-            answer = Math.min(answer, cnt);
-            return;
-        }
-        if(cnt > answer) return;
+    public int bfs(String begin, String target, String[] words) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(begin, 0));
         
-        for(int i=0; i<words.length; i++) {
-            if(!v[i] && isPossible(cur, words[i])) {
-                v[i] = true;
-                dfs(words[i], target, words, cnt+1);
-                v[i] = false;
+        while(!q.isEmpty()) {
+            Node cur = q.poll();
+            // System.out.println("-----------------------------");
+            // System.out.println(cur.word);
+            // System.out.println(cur.cnt);
+            // System.out.println("-----------------------------");
+            if(cur.word.equals(target)) return cur.cnt;
+            
+            for(int i=0; i<words.length; i++) {
+                if(!v[i] && !words[i].equals(cur.word) && isPossible(cur.word, words[i])){
+                    q.add(new Node(words[i], cur.cnt+1));
+                    v[i] = true;
+                }
             }
         }
+        
+        return 0;
     }
     
-    public boolean isPossible(String cur, String diff) {
+    public boolean isPossible(String cur, String word) {
         int cnt = 0;
         for(int i=0; i<cur.length(); i++) {
-            if(cur.charAt(i) == diff.charAt(i)) cnt++;
+            if(cur.charAt(i) != word.charAt(i)) {
+                cnt++;
+            }
+            if(cnt > 1) return false;
         }
         
-        return cnt + 1 == cur.length();
+        return true;
+    }
+    
+    class Node {
+        String word;
+        int cnt;
+        public Node (String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
+        }
     }
 }
